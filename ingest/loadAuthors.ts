0f3a6d7618613923442e162c7d1b5614a918b514
rickjohnson <richardpatrickjohnson@gmail.com>
2019-11-19 15:24:44 -0500

    pushing institutions and persons for harper center

--- ingest/loadAuthors.ts
+++ ingest/loadAuthors.ts
@@ -12,12 +12,12 @@ import { command as loadCsv } from './units/loadCsv'
 const INSTITUTION = "University of Notre Dame"
 
 const INSERT_INSTITUTION_MUTATION = gql`
-
-    mutation InsertInstitutionMutation ($institutions:[institutions_insert_input!]!){
+  mutation InsertInstitutionMutation ($institutions:[institutions_insert_input!]!){
     __typename
 
     insert_institutions(
       objects: $institutions
+      on_conflict: {constraint: institutions_name_key, update_columns: name}
     ) {
       returning {
         id
@@ -26,6 +26,25 @@ const INSERT_INSTITUTION_MUTATION = gql`
     }
   }`
 
+  const INSERT_PERSON_MUTATION = gql`
+  mutation InsertPersonMutation ($persons:[persons_insert_input!]!){
+    __typename
+
+    insert_persons(
+      objects: $persons
+    ) {
+      returning {
+        id,
+        given_name,
+        family_name,
+        email,
+        position_title,
+        institution {
+            name
+        }
+      }
+    }
+  }`
 
 
 const client = new ApolloClient({
@@ -47,25 +66,50 @@ async function go() {
     console.log(authors);
 
     //insert institutions first
-    const institutions = _.uniq(_.map(authors, 'INSTITUTION'))
+    const institutions = _.uniq(_.map(authors, 'institution'))
     console.log(institutions)
 
     const result = await client.mutate({
         mutation: INSERT_INSTITUTION_MUTATION,
         variables: {
-            institutions: _.map(institutions, i => ({ name: i}))
+            institutions: _.map(institutions, i => ({ name: i}), {})
         }
     });
 
-    console.log(result);
-
+    console.log(JSON.stringify(result.data));
     //get indexed id's for institutions, and update author list with id's for inserts
 
-    //console.log(result.data.returning)
+    const inserted_institutions = result.data["insert_institutions"]["returning"] || []
+    const institutionNameIdMap = _.reduce(inserted_institutions, (obj,inst) => {
+        if(inst.name && inst.id)
+            obj[inst['name']] = inst['id']
+        return obj
+    }, {})
+    // console.log(inserted_institutions)
+    // console.log(institutionNameIdMap)
 
-    //now add auth
+    //now add authors
+    const authors_with_ids = _.map(authors, author => {
+        const obj = {
+            "family_name": author["family_name"],
+            "given_name": author["given_name"],
+            "email": author["email"],
+            "position_title": author["position_title"]
+        }
+        if (institutionNameIdMap[author["institution"]]) 
+            obj["institution_id"] = institutionNameIdMap[author["institution"]]
+        return obj 
+    })
+    console.log(authors_with_ids)
 
-    //console.log(result)
+    const resultInsertAuthors = await client.mutate({
+        mutation: INSERT_PERSON_MUTATION,
+        variables: {
+            persons: authors_with_ids
+        }
+    });
+
+    console.log(resultInsertAuthors)
 }
 
 go();
