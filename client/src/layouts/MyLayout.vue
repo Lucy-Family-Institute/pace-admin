f400628dbf49f84f68b96819fe3e29600942cb89
Jeffrey Spies <code@jeffspies.com>
2019-11-20 11:39:39 -0500

    minor changes

--- client/src/layouts/MyLayout.vue
+++ client/src/layouts/MyLayout.vue
@@ -19,6 +19,37 @@
         <q-btn stretch flat label="Logs" />
         <q-space />
         <q-toggle v-model="isBulkEditing" color="red" label="Bulk Edit" left-label />
+        <q-btn-group unelevated spread>
+          <q-separator class="gt-sm" vertical inset/>
+          <q-btn
+            dense
+            flat
+            class="text-weight-light text-grey-8"
+          >
+            <q-avatar  size="32px">
+              <img src="https://cdn.quasar.dev/img/avatar1.jpg">
+            </q-avatar>
+            <q-menu>
+              <q-list style="min-width: 200px">
+              <q-item to="/settings" clickable v-close-popup>
+                <q-item-section>
+                  <q-item-label>
+                    <q-icon name="settings" class="q-pr-sm"/>Account Settings
+                  </q-item-label>
+                </q-item-section>
+              </q-item>
+              <q-separator inset/>
+              <q-item @click='logout' clickable v-close-popup>
+                <q-item-section>
+                  <q-item-label>
+                    <q-icon name="lock" class="q-pr-sm"/>Logout
+                  </q-item-label>
+                </q-item-section>
+              </q-item>
+            </q-list>
+            </q-menu>
+          </q-btn>
+        </q-btn-group>
       </q-toolbar>
     </q-header>
 
--- client/src/pages/Index.vue
+++ client/src/pages/Index.vue
@@ -82,16 +82,7 @@
               </q-scroll-area>
             </template>
             <template v-slot:after>
-              <q-scroll-area
-                v-if="publication"
-                :style="{height: ($q.screen.height-50)+'px'}"
-              >
-                <ul>
-                  <li v-for="link in links" :key="link.id"><a :href="link.url">Google the title</a></li>
-                  <li v-if="unpaywall"><a :href="unpaywall">PDF</a></li>
-                </ul>
-                <vue-friendly-iframe v-if="url" :src="url"></vue-friendly-iframe>
-              </q-scroll-area>
+              
             </template>
           </q-splitter>
         </template>
