28a26907c447ecf4cdd61e2b1f1c4a8e14782069
Jeffrey Spies <code@jeffspies.com>
2019-11-20 10:16:27 -0500

    Added crossref publication search

--- ingest/crossref.ts
+++ ingest/crossref.ts
@@ -1,18 +1,54 @@
 import axios from 'axios'
 import _ from 'lodash'
+import { ApolloClient, MutationOptions } from 'apollo-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { createHttpLink } from 'apollo-link-http'
+import fetch from 'node-fetch'
+import pEachSeries from 'p-each-series'
+import readUsers from '../client/src/gql/readUsers'
+import insertPaper from './gql/insertPaper'
+
+const client = new ApolloClient({
+  link: createHttpLink({
+    uri: 'http://localhost:8002/v1/graphql',
+    headers: {
+      'x-hasura-admin-secret': 'mysecret'
+    },
+    fetch: fetch as any
+  }),
+  cache: new InMemoryCache()
+})
 
 async function main() {
-  const result = await axios({
-    method: 'get',
-    url: 'https://api.crossref.org/works?query.author=Ani+Aprahamian',
-    headers: {
-      'User-Agent': 'GroovyBib/1.1 (https://example.org/GroovyBib/; mailto:GroovyBib@example.org) BasedOnFunkyLib/1.4'
+  const queryResult = await client.query(readUsers())
+  const simplifiedPersons = _.map(queryResult.data.persons, (person) => {
+    return {
+      id: person.id,
+      lastName: _.lowerCase(person.family_name),
+      firstInitial: _.lowerCase(person.given_name[0])
     }
   })
-  _.map(result.data.message.items, (item) => {
-    if(item.type === 'journal-article') {
-      console.log(item.title, item.DOI, item)
-    }
+
+  pEachSeries(simplifiedPersons, async (person) => {
+    const result = await axios({
+      method: 'get',
+      url: `https://api.crossref.org/works?query.author=${person.lastName}`,
+      headers: {
+        'User-Agent': 'GroovyBib/1.1 (https://example.org/GroovyBib/; mailto:GroovyBib@example.org) BasedOnFunkyLib/1.4'
+      }
+    })
+    _.map(result.data.message.items, (item) => {
+      if(item.type === 'journal-article') {
+        _.each(item.author, async (author) => {
+          if(_.lowerCase(author.family) === person.lastName && 
+            _.startsWith(_.lowerCase(author.given), person.firstInitial)
+          ) {
+            const mutateResult = await client.mutate(insertPaper(person.id, item.title[0], item.DOI))
+            console.log('Added!')
+          }
+        })
+      }
+    })
   })
 }
 
