4e383ef84fe6a13ae5a5c77c49f524bf166c64a6
Jeffrey Spies <code@jeffspies.com>
2019-11-21 08:46:34 -0500

    filtering and confidence

--- client/src/gql/readPublicationsByPerson.js
+++ client/src/gql/readPublicationsByPerson.js
@@ -4,10 +4,18 @@ export default function readPublicationsByPerson (id) {
   return {
     query: gql`
       query MyQuery ($person_id: Int!){
-        publications(where: {persons_publications: {person_id: {_eq: $person_id}}}) {
+        publications(
+          where: {
+            persons_publications: {person_id: {_eq: $person_id}}
+          },
+          order_by: {persons_publications_aggregate: {avg: {confidence: asc}}}
+        ) {
           id
           title
           doi
+          persons_publications {
+            confidence
+          }
         }
       }
     `,
