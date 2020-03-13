d6edbff416cded92041e5e93a80adbe71232eeb2
rickjohnson <richardpatrickjohnson@gmail.com>
2020-03-13 13:44:00 -0400

    fix bug with inserting review

--- client/src/gql/insertReview.js
+++ client/src/gql/insertReview.js
@@ -3,21 +3,21 @@ import gql from 'graphql-tag'
 export default function insertReview (userId, personPublicationId, reviewStateAbbrev) {
   return {
     mutation: gql`
-      mutation MyMutation($user_id: Int!, $persons_publication_id: Int!, $reviewstate_abbrev: String!) {
+      mutation MyMutation($user_id: Int!, $persons_publications_id: Int!, $reviewstate_abbrev: String!) {
         insert_reviews(objects: {
           reviewstate_abbrev: $reviewstate_abbrev, 
           user_id: $user_id, 
-          persons_publications_id: $persons_publication_id}) {
+          persons_publications_id: $persons_publications_id}) {
           returning {
             id,
-            person_publications_id
+            persons_publications_id
           }
         }
       }
     `,
     variables: {
       user_id: userId,
-      persons_publication_id: personPublicationId,
+      persons_publications_id: personPublicationId,
       reviewstate_abbrev: reviewStateAbbrev
     }
   }
