68bad395fbc40ec69c315ff4ad6c962f66ea3f69
rickjohnson <richardpatrickjohnson@gmail.com>
2020-03-24 10:56:16 -0400

    add sort person

--- client/src/components/PeopleFilter.vue
+++ client/src/components/PeopleFilter.vue
@@ -12,6 +12,17 @@
         class="fullSelect"
       />
     </q-item>
+    <q-item dense>
+      <q-select
+        name="sort_person"
+        v-model="selectedPersonSort"
+        :options="sortPersonOptions"
+        color="primary"
+        filled
+        label="Sort Person By:"
+        class="fullSelect"
+      />
+    </q-item>
   </div>
 </template>
 
@@ -24,11 +35,17 @@ import readInstitutions from '../../../gql/readInstitutions.gql'
 export default {
   data () {
     return {
-      institutionOptions: []
+      institutionOptions: [],
+      sortPersonOptions: [
+        'Total',
+        'Name'
+      ],
+      preferredPersonSort: 'Total'
     }
   },
   computed: {
-    selectedInstitutions: sync('filter/selectedInstitutions')
+    selectedInstitutions: sync('filter/selectedInstitutions'),
+    selectedPersonSort: sync('filter/selectedPersonSort')
   },
   async created () {
     this.fetchData()
@@ -43,6 +60,7 @@ export default {
       })
       this.institutionOptions = _.compact(_.map(results.data.institutions, 'name'))
       this.selectedInstitutions = _.clone(this.institutionOptions)
+      this.selectedPersonSort = this.preferredPersonSort
     }
   }
 }
