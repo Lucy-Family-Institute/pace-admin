351849a0a713a81d4cc2202da58e05be7a1146f8
rickjohnson <richardpatrickjohnson@gmail.com>
2020-04-05 09:15:39 -0400

    initial checkin some pubmed records going into db

---
+++ ingest/fetchPubmedData.js
@@ -0,0 +1,184 @@
+const _ = require('lodash');
+const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+const pify = require('pify');
+const pMap = require('p-map');
+const schema = require('schm');
+const translate = require('schm-translate');
+const xmlToJson = require('xml-js');
+
+const getIds = require('./units/joinCsvs').command;
+
+const dataFolderPath = "../data";
+
+//const getText = constraints => prevSchema => prevSchema.merge({
+//  validate(values) {
+//    const parsed = prevSchema.parse(values);
+//    return parsed._text;
+//  }
+//});
+
+const creatorSchema = schema({
+  affiliation: {type: String, default: null},
+  givenName: {type: String, default: null},
+  familyName: {type: String, default: null},
+  initials: {type: String, default: null},
+}, translate({
+  affiliation: 'AffiliationInfo.Affiliation._text',
+  givenName: 'ForeName._text',
+  familyName: 'LastName._text',
+  initials: 'Initials._text'
+}));
+
+const resourceIdentifierSchema = schema({
+  resourceIdentifierType: {type: String, default: null},
+  resourceIdentifier: {type: String, default: null},
+}, translate({
+  resourceIdentifierType: '_attributes.IdType',
+  resourceIdentifier: '_text',
+}));
+
+const funderIdentifierSchema = schema({
+  funder: {type: String, default: null},
+  country: {type: String, default: null},
+  funderIdentifier: {type: String, default: null},
+}, translate ({
+  funder: 'Agency._text',
+  country: 'Country._text',
+  funderIdentifier: 'GrantID._text',
+}));
+
+const shareWorkSchema = schema({
+  title: {type: String, default: null},
+  description: {type: String, default: null},
+  creators: [creatorSchema],
+  resourceIdentifiers: [resourceIdentifierSchema],
+  funderIdentifiers: [funderIdentifierSchema],
+}, translate({
+  title: 'MedlineCitation.Article.ArticleTitle._text',
+  description: 'MedlineCitation.Article.Abstract.AbstractText._text',
+  creators: 'MedlineCitation.Article.AuthorList.Author',
+  resourceIdentifiers: 'PubmedData.ArticleIdList.ArticleId',
+  funderIdentifiers: 'MedlineCitation.Article.GrantList.Grant'
+}));
+ 
+async function wait(ms){
+  return new Promise((resolve, reject)=> {
+    setTimeout(() => resolve(true), ms );
+  });
+}
+
+async function getAwardPublications(awardId){
+  const ids = await getESearch(awardId);
+  const records = await getEFetch(ids);
+  if(_.get(records, 'PubmedArticleSet.PubmedArticle', null)) {
+    return extractMetadata(records);
+  }
+  return null;
+}
+
+async function getESearch(term){
+  await wait(1000);
+  const url = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi';
+  const response = await axios.get(url, {
+    params: {
+      db: 'pubmed',
+      retmode: 'json',
+      retmax: '100',
+      term,
+    }
+  });
+  return response.data.esearchresult.idlist;
+}
+
+async function getEFetch(ids){
+  await wait(1000);
+  const url = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi';
+  const commaSeparatedIds = _.join(ids, ',');
+  const response = await axios.get(url, {
+    responseType: 'text',
+    params: {
+      db: 'pubmed',
+      retmode: 'xml',
+      retmax: '100',
+      id: commaSeparatedIds,
+    }
+  });
+  return xmlToJson.xml2js(response.data, {compact:true});
+}
+
+function extractMetadata(rawJson){
+  if (_.isArray(rawJson.PubmedArticleSet.PubmedArticle)) {
+    return _.map(rawJson.PubmedArticleSet.PubmedArticle, (value,key)=> {
+      return shareWorkSchema.parse(value);
+    });
+  } else {
+      return shareWorkSchema.parse(rawJson.PubmedArticleSet.PubmedArticle);
+  }
+}
+
+async function getFileData(filePath){
+  //incomplete...
+  fs.readFile('/etc/passwd', (err, data) => {
+    if (err) throw err;
+    console.log(data);
+  });
+}
+
+async function createAwardPubJSON(awardId, data){
+
+}
+
+//async function loadAwardIdPublications(awardDataDir){
+//
+//  if (awardDataDir){
+//    console.log(`Reading files from directory: ${awardDataDir}`);
+//    fs.readdir(awardDataDir, (err, files) => {
+//      if (err) throw err;
+//      const mapper = async (fileName) => {
+//        const filePath = path.join(awardDataDir,`${filename}`);
+//        console.log(`Reading data from file: ${filePath}`);
+//        awardPubs = [];
+ //       const data = await getFileData(filePath);
+ //       if (data){
+ //         if (filename.includes('.')){
+ //           awardId = filename.split('.').slice(0,-1).join('.');
+ //         } else {
+ //           awardId = filename;
+ //         }
+ //         console.log(`Creating object for award id: ${awardId}`);
+ //         awardPub = createJsonObject(awardId, data);
+ //         awardPubs.push(awardPub);
+ //       }
+ //     };
+ //   }
+ // } else {
+ //   console.log('Reading data from Directory failed: File directory undefined');
+ // }
+  
+  
+//}
+
+async function go() {
+  const awardIds = await getIds();
+  console.log(`Award ids are: ${JSON.stringify(awardIds, null, 2)}`)
+  //const uniqueAwardIds = _.uniq(awardIds);
+  let uniqueAwardIds = ['GM067079','CA212964'];
+  console.log(`Found ${awardIds.length} awards; ${uniqueAwardIds.length} unique`);
+
+  const mapper = async (awardId) => {
+    console.log(`Working on ${awardId}`);
+    const response = await getAwardPublications(awardId);
+    const filename = path.join(process.cwd(), dataFolderPath, 'awards', `${awardId}.json`);
+    if( response ) {
+      console.log(`Writing ${filename}`);
+      await pify(fs.writeFile)(filename, JSON.stringify(response));
+    }
+  };
+  //actually run the method above to getAwardPublications against each awardId in the awardsIds array, timeout enabled to avoid exceeded request limit to PMC
+  const result = await pMap(uniqueAwardIds, mapper, {concurrency: 2});
+}
+
+go();
+
---
+++ ingest/joinAuthorAwards.js
@@ -0,0 +1,169 @@
+const _ = require('lodash');
+const pify = require('pify');
+const fs = require('fs');
+const path = require('path');
+const pMap = require('p-map');
+const moment = require('moment');
+
+const writeCsv = require('./units/writeCsv').command;
+const loadCsv = require('./units/loadCsv').command;
+const fuzzyMatchName = require('./units/fuzzyMatchName').command;
+const nameParser = require('./units/nameParser').command;
+
+// return map of identifier type to id
+function getResourceIdentifiers (resourceIdentifiers) {
+  console.log(`Keying resource identifiers by type: ${JSON.stringify(resourceIdentifiers, null,2)}`)
+  return _.keyBy(resourceIdentifiers, 'resourceIdentifierType')
+}
+
+async function mapGrantFiles (filename) {
+  if(!_.endsWith(filename, '.json')) return;
+  const data = await pify(fs.readFile)(path.join('../data', 'awards2', filename));
+  const grantId = filename.replace('.json', '');
+
+  console.log(`Processing Grant Award Id: ${grantId}`)
+
+  let jsonObj = JSON.parse(data);
+  if( !_.isArray(jsonObj) )
+    jsonObj = [jsonObj];
+
+  const mappedOverObject = await pMap(jsonObj, async (pub) => {
+    const title = pub.title;
+    const identifiers = getResourceIdentifiers(pub.resourceIdentifiers)
+    console.log(`Processing Pub: ${JSON.stringify(pub, null, 2)}`)
+    console.log(`Found Resource Identifiers for Title: ${title} ids: ${JSON.stringify(identifiers, null, 2)}`)
+    let creators = ''
+    const mappedData = await pMap(pub.creators, async (creator, index) => {
+
+      if (index > 0) {
+        creators = `${creators};`
+      }
+      creators = `${creators}${creator.familyName}, ${creator.givenName}`
+    }, { concurrency: 1 });
+
+    const harper = await loadCsv({
+      path: '../data/harper.csv',
+    });
+
+    const parsedName = await nameParser({
+      name: `${pub.creators[0].givenName} ${pub.creators[0].familyName}`,
+      reduceMethod: 'majority',
+    });
+
+    const isHarperResult = fuzzyMatchName({
+      first: parsedName.first,
+      last: parsedName.last,
+      corpus: harper,
+      keyFirst: 'firstname',
+      keyLast: 'lastname',
+    });
+
+    const awards = await loadCsv({
+      path: '../data/awards14-18.csv',
+    });
+
+    const investigatorCorpus = await pMap(awards, async (award) => {
+      const name = award['Award Investigator Full Name'];
+      const nameObj = await nameParser({
+        name,
+        reduceMethod: 'majority',
+      });
+      return nameObj;
+    }, {concurrency: 1});
+
+    const isAwardInvestigatorResult = fuzzyMatchName({
+      first: parsedName.first,
+      last: parsedName.last,
+      corpus: investigatorCorpus,
+      keyFirst: 'first',
+      keyLast: 'last',
+    });
+    
+    const leadCorpus = await pMap(awards, async (award) => {
+      const name = award['Award Lead Investigator Name'];
+      const nameObj = await nameParser({
+        name,
+        reduceMethod: 'majority',
+      });
+      return nameObj;
+    }, {concurrency: 1});
+
+    const isLeadInvestigatorResult = fuzzyMatchName({
+      first: parsedName.first,
+      last: parsedName.last,
+      corpus: leadCorpus,
+      keyFirst: 'first',
+      keyLast: 'last',
+    },  {concurrency: 1});
+    
+    let doi = identifiers.doi ? identifiers.doi.resourceIdentifier : ''
+    let pubmedId = identifiers.pubmed ? identifiers.pubmed.resourceIdentifier: ''
+    return {
+      pubTitle: title,
+      grantId: grantId,
+      nihGivenName: pub.creators[0].givenName,
+      nihFamilyName: pub.creators[0].familyName,
+      doi: doi,
+      pubmed_id: pubmedId,
+      pubmed_record: JSON.stringify(pub),
+      first: parsedName.first,
+      last: parsedName.last,
+      fullName: `${parsedName.first} ${parsedName.last}`,
+      nihAffiliation: pub.creators[0].affiliation,
+      authorPosition: 1, // index + 1,
+      isFirstAuthor: true, //index === 0,
+      isLastAuthor: (pub.creators.length - 1) === 1, // index,
+      isHarper: isHarperResult !== null,
+      isInvestigator: isAwardInvestigatorResult !== null,
+      isLeadInvestigator: isLeadInvestigatorResult !== null,
+    };
+    return mappedData;
+  }, { concurrency: 1 });
+  return _.flatten(mappedOverObject);
+}
+
+function leftOuterJoin(left, leftKey, right, rightKey) {
+  const rightKeyed = _.keyBy(right, rightKey);
+  return _.map(left, (leftObj) => {
+    return _.merge(leftObj, rightKeyed[leftObj[leftKey]])
+  });
+}
+
+async function go() {
+
+  console.log('Processing NIH load')
+  const awards = await loadCsv({
+    path: '../data/awards14-18.csv',
+  });
+  
+  const nih = _.compact(_.map(awards, (award) => {
+    if(!_.startsWith(award['Prime Sponsor'], 'National Institutes of Health')) {
+      return null;
+    }
+    const awardId = award['Sponsor Award Number/ Award ID'];
+    const reResult = /\w{4}([a-z]{2}[0-9]{6})-?.{2,4}.*/i.exec(awardId);
+    if(!reResult) return null;
+    award.grantId = reResult[1];
+    return award;
+  }));
+  
+  console.log('Reading awards')
+  const files = await pify(fs.readdir)('../data/awards2');
+
+  console.log ('Mapping grant files')
+  const authorsByGrant = await pMap(files, mapGrantFiles, { concurrency: 1 });
+
+  console.log('Flattening grant files')
+  const authors = _.compact(_.flatten(authorsByGrant));
+
+  console.log('Joining Grant Award Data')
+  const data = leftOuterJoin(authors, 'grantId', nih, 'grantId');
+
+  console.log('Writing Grant Award data to disk')
+  await writeCsv({
+    path: `../data/authorsByAwards.${moment().format('YYYYMMDDHHmmss')}.csv`,
+    data,
+  });
+}
+
+go();
