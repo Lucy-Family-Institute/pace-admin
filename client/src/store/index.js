53ac3209c4cc351918b094667443a101e4f14926
rickjohnson <richardpatrickjohnson@gmail.com>
2020-05-05 11:56:07 -0400

    added saving filter settings

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+// import VuexPersist from 'vuex-persist'
+import createPersistedState from 'vuex-persistedstate'
+// import { localForageService } from './localForage'
 
 import pathify from './pathify'
 
@@ -9,6 +12,40 @@ import filter from './filter'
 
 Vue.use(Vuex)
 
+// const vuexLocalStorage = new VuexPersist({
+//   key: 'vuex', // The key to store the state on in the storage provider.
+//   storage: window.localStorage // or window.sessionStorage or localForage
+//   // Function that passes the state and returns the state with only the objects you want to store.
+//   // reducer: state => state,
+//   // Function that passes a mutation and lets you decide if it should update the state in localStorage.
+//   // filter: mutation => (true)
+// })
+
+// const vuexLocal = new VuexPersist({
+//   // vuex-persist
+//   storage: localForageService, // localForage
+//   asyncStorage: true,
+//   key: 'vuexPersistStorage_test',
+//   supportCircular: true,
+//   saveState: async (key, state, storage) => {
+//     let data = state
+
+//     if (storage && data) {
+//     }
+//     storage.setItem(key, data)
+//   },
+//   restoreState: async function (key, storage) {
+//     let data = await storage.getItem(key)
+//     if (await data) {
+//       try {
+//       } catch (e) {
+//         console.log(e)
+//       }
+//     }
+//     return data
+//   }
+// })
+
 /*
  * If not building with SSR mode, you can
  * directly export the Store instantiation
@@ -16,7 +53,9 @@ Vue.use(Vuex)
 
 export default function (/* { ssrContext } */) {
   const Store = new Vuex.Store({
-    plugins: [ pathify.plugin ],
+    plugins: [ createPersistedState({
+      paths: ['filter']
+    }), pathify.plugin ],
 
     modules: {
       admin,
