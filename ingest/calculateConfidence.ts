4f8bda0771b5ff81da9245ce731b544248a76189
rickjohnson <richardpatrickjohnson@gmail.com>
2020-05-09 07:35:23 -0400

    incremental check in granular confidence checking

--- ingest/calculateConfidence.ts
+++ ingest/calculateConfidence.ts
@@ -120,13 +120,209 @@ async function getConfidenceTypesByRank() {
   return confidenceTypesByRank
 }
 
-async function main() {
+async function calculateConfidenceByRank (personPublication, confidenceTypes) {
+
+}
+
+async function calculatePaperConfidence (personPublication, confidenceTypesByRank) {
+  const confidenceValue = 0.0
+  // const addConfidenceByRank = calculateConfidenceByRank (personPublication)
+}
+
+async function getCSLAuthors(paperCsl){
+
+  const authMap = {
+    firstAuthors : [],
+    otherAuthors : []
+  }
+  
+  let authorCount = 0
+  //console.log(`Before author loop paper csl: ${JSON.stringify(paperCsl,null,2)}`)
+  _.each(paperCsl.author, async (author) => {
+    // skip if family_name undefined
+    if (author.family != undefined){
+      //console.log(`Adding author ${JSON.stringify(author,null,2)}`)
+      authorCount += 1
+            
+      //if given name empty change to empty string instead of null, so that insert completes
+      if (author.given === undefined) author.given = ''
+
+      if (_.lowerCase(author.sequence) === 'first' ) {
+        //console.log(`found first author ${ JSON.stringify(author) }`)
+        authMap.firstAuthors.push(author)
+      } else {
+        //console.log(`found add\'l author ${ JSON.stringify(author) }`)
+        authMap.otherAuthors.push(author)
+      }
+    }
+  })
+
+  //add author positions
+  authMap.firstAuthors = _.forEach(authMap.firstAuthors, function (author, index){
+    author.position = index + 1
+  })
+
+  authMap.otherAuthors = _.forEach(authMap.otherAuthors, function (author, index){
+    author.position = index + 1 + authMap.firstAuthors.length
+  })
+
+  //concat author arrays together
+  const authors = _.concat(authMap.firstAuthors, authMap.otherAuthors)
+
+  //console.log(`Author Map found: ${JSON.stringify(authMap,null,2)}`)
+  return authors
+}
+
+async function getPublicationAuthorMap (publicationCsl) {
+  //retrieve the authors from the record and put in a map, returned above in array, but really just one element
+  const authors = await getCSLAuthors(publicationCsl)
+  // group authors by last name
+  //create map of last name to array of related persons with same last name
+  const authorMap = _.transform(authors, function (result, value) {
+    const lastName = value.family.toLowerCase()
+    return (result[lastName] || (result[lastName] = [])).push(value)
+  }, {})
+  return authorMap
+}
+
+async function testAuthorLastName (author, publicationAuthorMap) {
+  //check for any matches of first initial or affiliation
+  const authorLastName = author.lastName.toLowerCase()
+  return _.hasIn(publicationAuthorMap, authorLastName)
+}
+
+async function testConfirmedAuthor (author, publication) {
+  //check if author in confirmed list and change confidence to 0.99 if found
+  if (publication.confirmedAuthorMap && publication.confirmedAuthorMap.length > 0){
+    const authorLastName = author.lastName.toLowerCase()
+    _.each(publication.confirmedAuthorMap[authorLastName], function (confirmedAuthor){
+      if (confirmedAuthor.lastName.toLowerCase() === author.lastName.toLowerCase() &&
+      confirmedAuthor.firstName.toLowerCase() === author.firstName.toLowerCase()){
+        console.log(`Confirmed author found: ${JSON.stringify(author,null,2)}, making confidence 0.99`)
+        return true
+      }
+    })
+  }
+  return false
+}
+
+// only call this method if last name matched
+async function testAuthorGivenNameInitial (author, publicationAuthorMap) {
+  //check for any matches of first initial or affiliation
+  const authorLastName = author.lastName.toLowerCase()
+  _.each(publicationAuthorMap[authorLastName], async (testPerson) => {
+    if (testPerson.given.toLowerCase()[0] === author.firstInitial.toLowerCase()){
+      return true
+    }
+  })
+  return false
+}
+
+// only call this method if last name matched
+async function testAuthorGivenName (author, publicationAuthorMap) {
+  //check for any matches of first initial or affiliation
+  const authorLastName = author.lastName.toLowerCase()
+  _.each(publicationAuthorMap[authorLastName], async (testPerson) => {
+    if (testPerson.given.toLowerCase() === author.firstName.toLowerCase()){
+      return true
+    }
+  })
+  return false
+}
+
+// assumes passing in authors that matched previously
+async function testAuthorAffiliation (author, publicationAuthorMap) {
+  const authorLastName = author.lastName.toLowerCase()
+  _.each(publicationAuthorMap[authorLastName], async (testPerson) => {
+    if(!_.isEmpty(testPerson.affiliation)) {
+      if(/notre dame/gi.test(testPerson.affiliation[0].name)) {
+        return true
+      }
+    }
+  })
+  return false
+}
+
+// returns true/false from a test called for the specific name passed in
+async function performConfidenceTest (confidenceTypeName, publication, author, publicationAuthorMap){
+  if (confidenceTypeName === 'lastname') {
+    return testAuthorLastName(author, publicationAuthorMap)
+  } else if (confidenceTypeName === 'confirmed_by_author') {
+    // needs to test against confirmed list
+    return testConfirmedAuthor(author, publication)
+  } else if (confidenceTypeName === 'given_name_initial') {
+    return testAuthorGivenNameInitial(author, publicationAuthorMap)
+  } else if (confidenceTypeName === 'given_name') {
+    return testAuthorGivenName(author, publicationAuthorMap)
+  } else if (confidenceTypeName === 'university_affiliation') {
+    return testAuthorAffiliation(author, publicationAuthorMap)
+  } else if (confidenceTypeName === 'common_coauthor') {
+    // need the publication for this test
+    // do nothing for now
+    return false
+  } else if (confidenceTypeName === 'subject_area') {
+    // do nothing for now
+    return false
+  }
+}
+
+async function calculateConfidenceAuthor (author, publication, confirmedAuthors, confidenceTypesByRank) {
+  const confidenceVal = 0.0
+  // array of arrays for each rank sorted 1 to highest number
+  // iterate through each group by rank if no matches in one rank, do no execute the next rank
+  const sortedKeys = _.sortBy(_.keys(confidenceTypesByRank), (value) => { return value })
+  // now just push arrays in order into another array
+
+  //update to current matched authors before proceeding with next tests
+  const publicationAuthorMap = getCSLAuthors(publication.csl)
+  _.each(sortedKeys, (key) => {
+    let matchFound = false
+    _.each(confidenceTypesByRank[key], (confidenceType) => {
+      // need to update to make publicationAuthorMap be only ones that matched last name for subsequent tests
+      const testConfidence = performConfidenceTest(confidenceType.name, publication, author, publicationAuthorMap)
+      if (testConfidence){
+        // log the confidence set to true in DB
+        matchFound = true
+        // calculate the confidence to add and add to overall value
+      }
+    })
+    if (!matchFound){
+      // stop processing and skip next set of tests
+      return confidenceVal
+    }
+  })
+}
+
+async function calculateConfidence (personMap, authors, confirmedAuthors) {
   // get the set of tests to run
   const confidenceTypesByRank = await getConfidenceTypesByRank()
   console.log(`Confidence Types By Rank: ${JSON.stringify(confidenceTypesByRank, null, 2)}`)
 
+  const confidenceMetricsByRank = {
+    1:  {
+      base: 0.3,
+      additiveCoefficient: 0.5
+    },
+    2: {
+      base: 0.2,
+      additiveCoefficient: 0.5,
+    },
+    3: {
+      base: 0.15,
+      additiveCoefficient: 0.6
+    }
+  }
+  // array of arrays for each rank sorted 1 to highest number
   // iterate through each group by rank if no matches in one rank, do no execute the next rank
   const sortedKeys = _.sortBy(_.keys(confidenceTypesByRank), (value) => { return value })
+  // now just push arrays in order into another array
+
+  //update to current matched authors before proceeding with next tests
+
+}
+
+async function main() {
+
   
 }
 
