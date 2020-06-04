ae7c1b0862ad0efdc5791620a03b52d0cc7c76b7
rickjohnson <richardpatrickjohnson@gmail.com>
2020-06-04 16:54:27 -0400

    add organization for ND and HCRI review categories

--- client/src/gql/insertReview.js
+++ client/src/gql/insertReview.js
@@ -1,13 +1,14 @@
 import gql from 'graphql-tag'
 
-export default function insertReview (userId, personPublicationId, reviewType) {
+export default function insertReview (userId, personPublicationId, reviewType, reviewOrganization) {
   return {
     mutation: gql`
-      mutation MyMutation($user_id: Int!, $persons_publications_id: Int!, $reviewType: type_review_enum!) {
+      mutation MyMutation($user_id: Int!, $persons_publications_id: Int!, $reviewType: type_review_enum!, $review_organization_value: review_organization_enum!) {
         insert_reviews(objects: {
           reviewType: $reviewType,
           user_id: $user_id, 
-          persons_publications_id: $persons_publications_id}) {
+          persons_publications_id: $persons_publications_id,
+          review_organization_value: $review_organization_value}) {
           returning {
             id,
             persons_publications_id
@@ -18,7 +19,8 @@ export default function insertReview (userId, personPublicationId, reviewType) {
     variables: {
       user_id: userId,
       persons_publications_id: personPublicationId,
-      reviewType
+      reviewType,
+      review_organization_value: reviewOrganization
     }
   }
 }
