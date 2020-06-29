bef43b71c36bddb6f068dd63b8e97d79b55aa33e
rickjohnson <richardpatrickjohnson@gmail.com>
2020-06-29 14:48:16 -0400

    changed drawer in dashboard to a splitter

--- dashboard-client/src/pages/Index.vue
+++ dashboard-client/src/pages/Index.vue
@@ -1,48 +1,51 @@
 <template>
   <q-page class="flex justify-center no-wrap">
-    <q-drawer
-        v-model="drawer"
-        show-if-above
-        side="left"
-        :mini-width=325
-        :mini="dashboardMiniState"
-
-        :width="$q.screen.width * .8"
-        :breakpoint="800"
-        bordered
-        content-class="bg-grey-3"
-      >
-       <div class="absolute" style="top: 15px; right: -17px">
-          <q-btn
+    <q-splitter
+      v-model="firstModel"
+      unit="px"
+      :style="{height: ($q.screen.height-56-16)+'px'}"
+      disable
+    >
+      <template v-slot:before>
+        <div class="q-pa-md row items-start q-gutter-md">
+          <q-card class="my-card" flat bordered>
+            <q-card-section>
+              <apexchart :width="`${(dashboardMiniState) ? 250: $q.screen.width * .25}`" type="bar" :options="yearOptions" :series="yearSeries"></apexchart>
+            </q-card-section>
+          </q-card>
+          <q-card class="my-card" flat bordered>
+            <q-card-section>
+              <apexchart :width="`${(dashboardMiniState) ? 250: $q.screen.width * .25}`" type="pie" :options="journalTypeOptions" :series="journalTypeSeries"></apexchart>
+            </q-card-section>
+          </q-card>
+          <q-card class="my-card" flat bordered>
+            <q-card-section>
+              <apexchart :width="`${(dashboardMiniState) ? 250: $q.screen.width * .25}`" type="bar" :options="classificationOptions" :series="classificationSeries"></apexchart>
+            </q-card-section>
+          </q-card>
+          <q-card class="my-card" flat bordered>
+            <q-card-section>
+              <apexchart :width="`${(dashboardMiniState) ? 250: $q.screen.width * .25}`" type="bar" :options="journalOptions" :series="journalSeries"></apexchart>
+            </q-card-section>
+          </q-card>
+        </div>
+      </template>
+      <template v-slot:separator>
+        <q-btn
             dense
             round
             unelevated
             color="teal"
             :icon="`${(dashboardMiniState) ? 'chevron_right' : 'chevron_left'}`"
-            @click="dashboardMiniState = !dashboardMiniState"
+            @click="toggleMiniState"
           />
+      </template>
+      <template v-slot:after>
+        <div>
+          <SearchView />
         </div>
-    <div class="q-pa-md row items-start q-gutter-md">
-      <q-card class="my-card" flat bordered>
-        <q-card-section>
-          <apexchart :width="`${(dashboardMiniState) ? 250: $q.screen.width * .25}`" type="bar" :options="yearOptions" :series="yearSeries"></apexchart>
-        </q-card-section>
-      </q-card>
-      <q-card class="my-card" flat bordered>
-        <q-card-section>
-          <apexchart :width="`${(dashboardMiniState) ? 250: $q.screen.width * .25}`" type="pie" :options="journalTypeOptions" :series="journalTypeSeries"></apexchart>
-        </q-card-section>
-      </q-card>
-      <q-card class="my-card" flat bordered>
-        <q-card-section>
-          <apexchart :width="`${(dashboardMiniState) ? 250: $q.screen.width * .25}`" type="bar" :options="options" :series="series"></apexchart>
-        </q-card-section>
-      </q-card>
-    </div>
-      </q-drawer>
-      <div>
-        <SearchView />
-      </div>
+      </template>
+    </q-splitter>
   </q-page>
 </template>
 
@@ -58,6 +61,7 @@ export default {
   },
   data () {
     return {
+      firstModel: this.$q.screen.width * 0.8,
       search: '',
       drawer: true,
       processingTime: undefined,
@@ -88,6 +92,10 @@ export default {
     yearSeries: sync('filter/yearSeries'),
     journalTypeOptions: sync('filter/journalTypeOptions'),
     journalTypeSeries: sync('filter/journalTypeSeries'),
+    classificationOptions: sync('filter/classificationOptions'),
+    classificationSeries: sync('filter/classificiationSeries'),
+    journalOptions: sync('filter/journalOptions'),
+    journalSeries: sync('filter/journalSeries'),
     dashboardMiniState: sync('filter/dashboardMiniState')
   },
   watch: {
@@ -101,17 +109,21 @@ export default {
     }
   },
   methods: {
-    drawerClick (e) {
+    toggleMiniState (e) {
       // if in "mini" state and user
       // click on drawer, we switch it to "normal" mode
       if (this.dashboardMiniState) {
         this.dashboardMiniState = false
+        this.firstModel = this.$q.screen.width * 0.8
 
         // notice we have registered an event with capture flag;
         // we need to stop further propagation as this click is
         // intended for switching drawer to "normal" mode only
-        e.stopPropagation()
+      } else {
+        this.dashboardMiniState = true
+        this.firstModel = this.$q.screen.width * 0.2
       }
+      // e.stopPropagation()
     },
     async init () {
       // const searchClient = new MeiliSearch({
