c86ba348d370c4dcea1e71656d89079e979fa9f6
rickjohnson <richardpatrickjohnson@gmail.com>
2020-08-03 22:52:23 -0400

    query tweaking

--- client/src/gql/readPersonPublicationsNDReviews.js
+++ client/src/gql/readPersonPublicationsNDReviews.js
@@ -1,39 +1,21 @@
 import gql from 'graphql-tag'
-import _ from 'lodash'
+// import _ from 'lodash'
 
-export default function readPersonPublicationsNDReviews (institutionNames, pubYearMin, pubYearMax, memberYearMin, memberYearMax) {
-  const startDateLT = `1/1/${memberYearMax + 1}`
-  const endDateGT = `12/31/${memberYearMin - 1}`
-  let namesString = ''
+export default function readPersonPublicationsNDReviews (personPubIds) {
+  const idsString = JSON.stringify(personPubIds)
+  // let idsString = ''
   // for now manually construct the string for names in the array
-  _.forEach(institutionNames, (value, index) => {
-    if (index > 0) {
-      namesString = `${namesString},`
-    }
-    namesString = `${namesString}"${value}"`
-  })
+  // _.forEach(personPubIds, (value, index) => {
+  //   if (index > 0) {
+  //     idsString = `${idsString},`
+  //   }
+  //   idsString = `${idsString}"${value}"`
+  // })
 
   return gql`
       query MyQuery {
         persons_publications(
-          where: {
-            person: {
-              _and: [
-                {start_date: {_lt: "${startDateLT}"}}, 
-
-                {institution: {name: {_in: [${namesString}]}}},
-                {
-                  _or: [
-                    {end_date: {_gt: "${endDateGT}"}}, 
-                    {end_date: {_is_null: true}}
-                  ]
-                }
-              ]
-            }, 
-            publication: {
-              year: {_gte: "${pubYearMin}", _lte: "${pubYearMax}"}
-            }
-          },
+          where: {id: {_in: ${idsString}}}
           order_by: {confidence: desc, publication: {title: asc}}
         ) {
           id
