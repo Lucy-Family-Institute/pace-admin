dfbe73f220fe0f3c8861fcca6ef7543a5be943c2
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2020-11-12 12:26:57 -0500

    Adding normalizer functions with tests
    Throughout the ingest code-base there are a few flavors of string
    normalization.  The `removeSpaces()`, `normalizeTitle()`, and
    `normalizeString()` functions represent those permutations.
    
    The tests demonstrate how those permutations happen.

---
+++ ingest/units/normalizer.ts
@@ -0,0 +1,68 @@
+import _ from 'lodash'
+
+/**
+* When the given parameter is a string, return a new string with spaces removed.
+*
+* @param value A string (or any value)
+* @returns A new string with spaces removed (or the given non-String value)
+*/
+export function removeSpaces (value) {
+  if (_.isString(value)) {
+    return _.clone(value).replace(/\s/g, '')
+  } else {
+    return value
+  }
+}
+
+function normalizeTitle(value) {
+  if (_.isString(value)) {
+    return _.clone(value)
+      .replace(/^the /i, '')
+      .replace(' and ', '')
+  } else {
+    return value
+  }
+}
+
+/**
+* Returns a new string with diacritics and "special characters"
+* removed.  If the given `value` is not a string, it returns that value.
+*
+* @remarks See ./tests/normalizer.test.ts for examples
+*
+* @param value A string (or any value)
+* @param options The valid `options` are:
+*                 * removeSpaces: when true remove all spaces
+*                 * skipLower: when true skip lower case conversion
+*                 * normalizeTitle: when true remove " and " and prefix of "the "
+* @returns A new string or the given value
+*/
+export function normalizeString(value, options = {}) {
+  let skipLower = _.get(options, "skipLower", false)
+  let rmSpaces = _.get(options, "removeSpaces", false)
+  let titleNormalization = _.get(options, "normalizeTitle", false)
+  if (_.isString(value)) {
+    let newValue = _.clone(value)
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '') // Remove diacritics
+      .replace(/[\u2019]/g, '\u0027') // the u0027 also normalizes the curly apostrophe to the straight one
+      .replace(/[&\/\\#,+()$~%.'":*?<>{}!]/g,'') // remove periods and other remaining special characters
+
+    if (!skipLower) {
+      newValue = _.lowerCase(newValue)
+    }
+
+    if (titleNormalization) {
+      newValue = normalizeTitle(newValue)
+    }
+
+    const returningValue = newValue
+    if (rmSpaces) {
+      return removeSpaces(returningValue)
+    } else {
+      return returningValue
+    }
+  } else {
+    return value
+  }
+}
