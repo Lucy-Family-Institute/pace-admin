9be485e11aa9a318162a3dd072baadde361fd539
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2020-11-17 11:54:59 -0500

    Removing unused methods and commented code
    The lsp-mode compiler says that all of the removed methods are not
    called.  I'm also removing the commented out methods as they add to the
    chatter.  As someone new to the project, it's hard to discern what the
    purpose of the file is when there is commented out code AND uncalled
    functions.
    
    If this is not something to remove, that's fine, but I believe the file
    needs revisiting.

--- ingest/fetchScopusAuthorData.ts
+++ ingest/fetchScopusAuthorData.ts
@@ -1,19 +1,14 @@
 import _ from 'lodash'
-import { ApolloClient, MutationOptions } from 'apollo-client'
+import { ApolloClient } from 'apollo-client'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { createHttpLink } from 'apollo-link-http'
 import fetch from 'node-fetch'
 import pMap from 'p-map'
 import pTimes from 'p-times'
 import readPersonsByYear from '../client/src/gql/readPersonsByYear'
-import readPublicationsByPersonByConfidence from '../client/src/gql/readPublicationsByPersonByConfidence'
-import { command as loadCsv } from './units/loadCsv'
-import { split } from 'apollo-link'
-import cslParser from './utils/cslParser'
 import { command as writeCsv } from './units/writeCsv'
 import moment from 'moment'
 import dotenv from 'dotenv'
-import resolve from 'path'
 import { randomWait } from './units/randomWait'
 
 dotenv.config({
@@ -28,24 +23,6 @@ process.env.NODE_ENV = 'development';
 
 // uncomment below line to test this code against staging environment
 // process.env.NODE_ENV = 'staging';
-
-// config variables
-const config = require('../config/config.js');
-
-import Cite from 'citation-js'
-
-//takes in a DOI and returns a json object formatted according to CSL (citation style language)
-//https://citation.js.org/api/index.html
-async function fetchByDoi(doi) {
-  //initalize the doi query and citation engine
-  Cite.async()
-
-  //get CSL (citation style language) record by doi from dx.dio.org
-  const cslRecords = await Cite.inputAsync(doi)
-  //console.log(`For DOI: ${doi}, Found CSL: ${JSON.stringify(cslRecords,null,2)}`)
-
-  return cslRecords[0]
-}
 
 const hasuraSecret = process.env.HASURA_SECRET
 const graphQlEndPoint = process.env.GRAPHQL_END_POINT
@@ -81,91 +58,6 @@ async function getScopusAuthorData(authorGivenName, authorFamilyName, year, scop
 
       return response.data;
 
-}
-
-async function getScopusPaperData(doi){
-  const baseUrl = 'https://api.elsevier.com/content/search/scopus'
-
-  const affiliationId = "60021508"
-
-  //const authorQuery = (query) {
-  //  return {
-  //    "AF-ID("+ affiliationId + ")"
-  //  }
-  //}
-  const doiQuery = "DOI(" + doi + ")"
-
-  const response = await axios.get(baseUrl, {
-      headers: {
-        'X-ELS-APIKey' : elsApiKey,
-      },
-      params: {
-        query : doiQuery
-      }
-    });
-
-    return response.data;
-
-}
-
-async function getScopusPaperAbstractData (scopusId) {
-  const baseUrl = `https://api.elsevier.com/content/abstract/scopus_id/${scopusId}`
-
-  const response = await axios.get(baseUrl, {
-    headers: {
-      'httpAccept' : 'text/xml',
-      'X-ELS-APIKey' : elsApiKey,
-    }
-  });
-
-  //console.log(response.data)
-  return response.data;
-}
-
-async function getScopusPaperFullText (doi) {
-  const baseUrl = 'https://api.elsevier.com/content/article/eid/1-s2.0-S152500161830594X'
-
-  const fullUrl = baseUrl + doi
-
-
-    const response = await axios.get(baseUrl, {
-        headers: {
-          'httpAccept' : 'text/xml',
-          'X-ELS-APIKey' : elsApiKey,
-        }
-      });
-
-      //console.log(response.data)
-      return response.data;
-}
-
-async function getScopusAuthorAffiliation (scopusId) {
-  const baseUrl = 'https://api.elsevier.com/content/abstract/scopus_id/85059466526?field=author,affiliation'
-
-  //const fullUrl = baseUrl + doi
-
-
-    const response = await axios.get(baseUrl, {
-        headers: {
-          'httpAccept' : 'text/xml',
-          'X-ELS-APIKey' : elsApiKey,
-        }
-      });
-
-      //console.log(response.data)
-      return response.data;
-}
-
-async function getConfirmedDOIsByPerson(){
-  //get publications from DB that have confidence level 0.99 for some person
-  const queryResult = await client.query(readPublicationsByPersonByConfidence(0.9))
-
-  const personPubsByDoi = _.groupBy(queryResult.data.persons_publications, function (pub) {
-    return pub.publication.doi
-  })
-
-  //console.log(`Person Pubs by DOI confirmed count: ${_.keys(personPubsByDoi).length} person pubs are: ${JSON.stringify(personPubsByDoi,null,2)}`)
-  return personPubsByDoi
 }
 
 async function getSimplifiedPersons(year) {
@@ -322,93 +214,6 @@ async function main (): Promise<void> {
     console.log(`Get error messages: ${JSON.stringify(failedScopusPapers,null,2)}`)
 
   }, { concurrency: 1 })
-
-  // let loopCounter = 0
-  // // iterate through list of DOI's and...
-  // await pMap(_.keys(confirmedDOIsByPerson), async (doi) => {
-  //     console.log(`DOI is: ${ doi }`)
-  //   // fetch paper by DOI
-  //   try {
-
-  //     loopCounter += 1
-  //     randomWait(loopCounter)
-
-  //     const responseDoi = await getScopusPaperData(doi);
-  //     if( responseDoi ) {
-  //       //console.log(`Result found for DOI: ${ doi }`)
-  //       //console.log(JSON.stringify(responseDoi['search-results'],null,2));
-  //       //get paper scopus id
-  //       if ( responseDoi['search-results']['entry'] ){
-  //         if (responseDoi['search-results']['entry'][0]['dc:identifier']){
-  //           const paperScopusId = responseDoi['search-results']['entry'][0]['dc:identifier'].split(':')[1]
-  //           console.log(`DOI: ${doi} Paper Scopus ID Found: ${paperScopusId}`)
-
-  //           //get author data
-  //           const responseAbstract = await getScopusPaperAbstractData(paperScopusId);
-  //           if( responseAbstract ) {
-  //             console.log(`Response Author Data: ${JSON.stringify(responseAbstract,null,2)}`)
-  //           }
-  //         }
-  //       }
-  //     }
-  //   } catch (error){
-  //     console.log(`Error for DOI: ${ doi }`)
-  //     console.error(error)
-  //   }
-  // }, { concurrency: 3 })
-  //             //get author affiliation
-  //             const responseAuthorAffiliation = await getScopusAuthorAffiliation(paperScopusId);
-  //             if( responseAuthorAffiliation ) {
-  //               console.log('Here1 ')
-  //               const authors = _.map(responseAuthorAffiliation['abstracts-retrieval-response']['authors']['author'], function (value) {
-  //                 console.log(`Value is: ${ JSON.stringify(value,null,2)}`)
-  //                 console.log(`Surname: ${ JSON.stringify(value['ce:surname'])}`)
-  //                 const author = {
-  //                   'family' : value['ce:surname'],
-  //                   'given' : value['ce:given-name'],
-  //                   'givenInitials' : value['ce:initials'],
-  //                   'scopusId' : value['@auid'],
-  //                   'affiliationId' : value['affiliation']['@id']
-  //                 }
-  //                 return author
-  //               })
-  //               //console.log(JSON.stringify(responseFullText['full-text-retrieval-response'],null,2));
-  //               console.log(`Here: ${ JSON.stringify(responseAuthorAffiliation,null,2) }`);
-  //               console.log(`Here2: ${ JSON.stringify(authors,null,2) }`);
-
-  //               //   //push to datastore
-  //               //   //start with pushing to csv one row for each relevant author + doi + each author name variant and/or + orcid id + scopus id
-  //               //   //doi, paper title, author list (name+id)
-
-  //               // // match to target author for HCRI
-
-  //               // // get full text
-
-  //               // // mine for name of author variation
-
-  //               // // capture name variant and ids
-  //             }
-  //           }
-  //         }
-  //       }
-  //   } catch (error){
-  //     console.log(`Error for DOI: ${ key }`)
-  //     // console.error(error)
-  //   }
-  // })
-
-      //   //get full text
-      //   const responseFullText = await getScopusPaperFullText("10.1016/j.ymthe.2018.12.010");
-      //   if( responseFullText ) {
-      //     //console.log(JSON.stringify(responseFullText['full-text-retrieval-response'],null,2));
-      //     //console.log(responseFullText);
-      //   }
-
-      // //const responseFullText = await getFullText(10);
-      // //if( responseFullText ) {
-      // //  console.log(responseFullText);
-      // //}
-      // console.log(`Config is: ${JSON.stringify(config)}`)
   }
 
   main();
