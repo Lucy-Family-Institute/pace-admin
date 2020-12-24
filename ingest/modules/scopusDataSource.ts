380db47075f0c7aeaccc6fb56bf6adb6184e6348
rickjohnson <richardpatrickjohnson@gmail.com>
2020-12-24 09:29:15 -0500

    initial check in failing scopusDataSource fetch

--- ingest/modules/scopusDataSource.ts
+++ ingest/modules/scopusDataSource.ts
@@ -1,13 +1,43 @@
-class ScopusDataSource implements DataSource {
+import axios from 'axios'
 
-  dsConfig: DataSourceConfig 
+export class ScopusDataSource implements DataSource {
+
+  private dsConfig: DataSourceConfig 
 
   constructor (dsConfig: DataSourceConfig) {
     this.dsConfig = dsConfig
   }
      // assumes that if only one of startDate or endDate provided it would always be startDate first and then have endDate undefined
-  async getPublicationsByName(lastName: String, firstName: String, startDate?: Date, endDate?: Date): Promise<[]> {
-      return []
+  async getPublicationsByName(lastName: String, firstName: String, startDate: Date, endDate?: Date): Promise<[]> {
+    const scopusAffiliationId = this.dsConfig.affiliationId
+    let authorQuery = "AUTHFIRST("+ firstName +") and AUTHLASTNAME("+ lastName +")"
+    if (this.dsConfig.affiliationId){
+      authorQuery = authorQuery+" and AF-ID("+this.dsConfig.affiliationId 
+    } 
+
+    // need to make sure date string in correct format
+    const results = await this.fetchScopusQuery(authorQuery, startDate.toString(), this.dsConfig.pageSize, 0)
+    return []
+  }
+
+  async fetchScopusQuery(query, date, pageSize, offset){
+    console.log(`dsConfig is :${JSON.stringify(this.dsConfig, null, 2)}`)
+
+    console.log(`Querying scopus with date: ${date}, offset: ${offset}, and query: ${query}`)
+
+    const response = await axios.get(this.dsConfig.queryUrl, {
+        headers: {
+          'X-ELS-APIKey' : this.dsConfig.apiKey,
+        },
+        params: {
+          query : query,
+          date: date,
+          count: pageSize,
+          start: offset
+        }
+    })
+
+      return response.data
   }
 
   // returns an array of normalized publication objects given ones retrieved fron this datasource
@@ -18,5 +48,9 @@ class ScopusDataSource implements DataSource {
   //returns a machine readable string version of this source
   getSourceName() {
       return 'Scopus'
+  }
+
+  initialize() {
+
   }
 }
--- ingest/modules/test/scopusDataSource.test.ts
+++ ingest/modules/test/scopusDataSource.test.ts
@@ -1,22 +1,48 @@
-// import ScopusDataSource from '../scopusDataSource'
+import { ScopusDataSource } from '../scopusDataSource'
+import dotenv from 'dotenv'
+
+dotenv.config({
+  path: '../../../.env'
+})
+    
+// environment variables
+process.env.NODE_ENV = 'development';
 
 let ds: ScopusDataSource
 let dsConfig: DataSourceConfig
 
 beforeAll(async () => {
-  dsConfig = new DataSourceConfig({
-    SCOPUS_API_KEY: 'test',
-    SCOPUS_API_COOKIE: 'test',
-    SCOPUS_ARTICLE_URI_BASE: 'test'
-  })
+  dsConfig = {
+    baseUrl: 'https://www-scopus-com.proxy.library.nd.edu',
+    queryUrl: 'https://api.elsevier.com/content/search/scopus',
+    apiKey: process.env.SCOPUS_API_KEY,
+    sourceName: 'Scopus',
+    pageSize: 25,
+    affiliationId: '60021508'
+  }
   ds = new ScopusDataSource(dsConfig)
 })
+
+test('testing fetch scopus query with REST call', async () => {
+  expect.hasAssertions();
+  const lastName = 'zhang'
+  const firstName = 's'
+  const scopusAffiliationId = '60021508'
+  const authorQuery = "AUTHFIRST("+ firstName +") and AUTHLASTNAME("+ lastName+ ") and AF-ID(" + scopusAffiliationId + ")"
+  const results = await ds.fetchScopusQuery(authorQuery, 2019, 1000, 0)
+  console.log(`Fetch Query Results are: ${JSON.stringify(results, null, 2)}`)
+})
   
-  test('testing get publication author map from csl', async () => {
-    expect.hasAssertions();
-  
-    // _.each(_.keys(pubCSLs), async (doi) => {
-    //   const foundPubAuthorMap = await getPublicationAuthorMap(pubCSLs[doi])
-    //   expect(pubAuthorMaps[doi]).toEqual(foundPubAuthorMap)
-    // })
-  })
+// test('testing get publication from Scopus with no start or end date set', async () => {
+//   expect.hasAssertions();
+
+// })
+
+// test('testing get publication from Scopus with start date set', async () => {
+//   expect.hasAssertions();
+
+//   const lastName = 'Aprahamian'
+//   const firstName = 'Ani'
+//   const results = await ds.getPublicationsByName(lastName, firstName, new Date('2017'))
+//   console.log(`Results are: ${JSON.stringify(results, null, 2)}`)
+// })
