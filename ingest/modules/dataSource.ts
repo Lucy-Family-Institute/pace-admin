49caad793b6f17b10d2b043fa4041b1240bace6f
rickjohnson <richardpatrickjohnson@gmail.com>
2020-12-30 09:24:27 -0500

    Add scopusDS getNormedPub test, TODO write method

--- ingest/modules/dataSource.ts
+++ ingest/modules/dataSource.ts
@@ -2,7 +2,7 @@ interface DataSource {
   // assumes that if only one of startDate or endDate provided it would always be startDate first and then have endDate undefined
   getPublicationsByAuthorName(person: NormedPerson, startDate?: Date, endDate?: Date) : Promise<[]>
   // returns an array of normalized publication objects given ones retrieved fron this datasource
-  getNormedPublications(sourcePublications: []): NormedPublication[]
+  getNormedPublications(sourcePublications: any[]): NormedPublication[]
 
   //returns a machine readable string version of this source
   getSourceName() : String
