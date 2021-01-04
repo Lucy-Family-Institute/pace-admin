404b6725e9c8d3c743851e76f0734e98d660cc06
rickjohnson <richardpatrickjohnson@gmail.com>
2021-01-04 11:20:14 -0500

    loadPublication unit, add stub code harvest.test

--- ingest/modules/test/harvester.test.ts
+++ ingest/modules/test/harvester.test.ts
@@ -1,11 +1,13 @@
 import { Harvester } from '../harvester'
 import { ScopusDataSource } from '../scopusDataSource'
+import { loadPublications} from '../../units/loadPublications'
 import dotenv from 'dotenv'
 const fs = require('fs');
 import _ from 'lodash'
 
 let scopusHarvester: Harvester
 let defaultNormedPerson: NormedPerson
+let expectedPublications: {}
 
 beforeAll(async () => {
 
@@ -42,11 +44,19 @@ beforeAll(async () => {
             scopusAffiliationId: '60021508'
         }
     }
-    
+
+    const testPersonsFilePath = '../../../data/persons_202101040758.csv'
+    const expectedPubCSVPath = '../../../data/scopus.2019.20210104110625.csv'
+    expectedPublications = await loadPublications(expectedPubCSVPath)
 })
 
+//TODO load in list of people to test against expected results for 2019
 test('test Scopus harvester.harvest', async () => {
     expect.hasAssertions()
     const results = await scopusHarvester.harvest([defaultNormedPerson], new Date('2019-01-01'))
+    // as new publications may be added to available, just test that current set includes expected pubs
+    // and that total harvested is in the same power of 10 and less than double the expected amount
     console.log(`Found publications: ${results.foundPublications.length}`)
 })
+
+//TODO write loadPublications test
