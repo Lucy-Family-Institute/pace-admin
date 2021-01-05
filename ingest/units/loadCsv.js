3dd1ed6f31d3c5db58f1e1b8c36d4b1049e524ee
rickjohnson <richardpatrickjohnson@gmail.com>
2021-01-05 08:51:40 -0500

    added lowerCaseColumn option to loadCsv

--- ingest/units/loadCsv.js
+++ ingest/units/loadCsv.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const Papa = require('papaparse');
+const _ = require('lodash');
 
 function parseCsv(file) {
   return new Promise((resolve, reject) => {
@@ -18,23 +19,43 @@ function parseCsv(file) {
   });
 }
 
-async function loadCsv(filePath) {
+async function loadCsv(filePath, lowerCaseColumns=false) {
   // TODO error on missing filepath
   console.log(`Loading CSV File from path: ${filePath}`)
   if (!fs.existsSync(filePath)) {
     throw `Invalid path on load csv from: ${filePath}`
   }
   const data = await parseCsv(fs.createReadStream(filePath));
-  return data;
+  if (lowerCaseColumns) {
+    return normalizeColumnsToLowercase(data);
+  } else {
+    return data;
+  }
+}
+
+/**
+ * 
+ * @param csvData An object expected to be rows loaded from a csv file
+ * 
+ * @returns The object with all columns converted to lowercase
+ */
+function normalizeColumnsToLowercase (rows) {
+  //normalize column names to all lowercase
+  return _.mapValues(rows, function (row) {
+    return _.mapKeys(row, function (value, key) {
+      return key.toLowerCase()
+    })
+  })
 }
 
 module.exports = {
   jsonSchema: {
     properties: {
       path: {},
+      lowerCaseColumns: false
     },
   },
   command: (input) => {
-    return loadCsv(input.path);
+    return loadCsv(input.path, input.lowerCaseColumns);
   },
 }
