89fbe4d5391b5652e5b6ed304ffa8d6fa5193fd0
rickjohnson <richardpatrickjohnson@gmail.com>
2021-01-07 16:59:05 -0500

    updated loadCSV to map results to new column names

--- ingest/units/test/loadCsv.test.ts
+++ ingest/units/test/loadCsv.test.ts
@@ -3,6 +3,63 @@ import _ from 'lodash'
 
 const testCSVPath = './test/fixtures/csv_test.csv'
 
+const columnMapNoLowercase = {
+  ID: 'id',
+  'Given_name': 'givenName',
+  'Family_Name': 'familyName',
+  'Start_Date': 'startDate',
+  'End_Date': 'endDate'
+}
+
+const expectedColumnMapNoLowercase = [
+  'id',
+  'givenName',
+  'familyName',
+  'Email',
+  'Position_Title',
+  'Institution_Id',
+  'startDate',
+  'endDate'
+]
+
+const columnMapLowercaseKeysOnly = {
+  'ID': 'id',
+  'Given_name': 'givenName',
+  'Family_Name': 'familyName',
+  'Start_Date': 'startDate',
+  'End_Date': 'endDate'
+}
+
+const expectedColumnMapLowercaseKeys = [
+  'id',
+  'givenName',
+  'familyName',
+  'Email',
+  'Position_Title',
+  'Institution_Id',
+  'startDate',
+  'endDate'
+]
+
+const columnMapAllLowercase = {
+  'id': 'id',
+  'given_name': 'givenname',
+  'family_name': 'familyname',
+  'start_date': 'startdate',
+  'end_date': 'enddate'
+}
+
+const expectedColumnMapAllLowercase = [
+  'id',
+  'givenname',
+  'familyname',
+  'Email',
+  'Position_Title',
+  'Institution_Id',
+  'startdate',
+  'enddate'
+]
+
 const expectedUnchangedColumns = [
   'id',
   'Given_name',
@@ -51,4 +108,30 @@ test('test column names not changed when loading csv and no forceLowerCaseColumn
     path: testCSVPath
   })
   expect(_.keys(csvData[0])).toEqual(expectedUnchangedColumns)
+})
+
+test('test load csv works with column map with no lowerCase keys or values', async () => {
+  expect.hasAssertions()
+  const csvData: any = await loadCsv({
+    path: testCSVPath,
+    columnNameMap: columnMapNoLowercase
+  })
+  expect(_.keys(csvData[0])).toEqual(expectedColumnMapNoLowercase)
+})
+
+test('test load csv works with column map with lowerCase columnNames and non lowerCase mapped column names', async () => {
+  expect.hasAssertions()
+  const csvData: any = await loadCsv({
+    path: testCSVPath,
+    columnNameMap: columnMapLowercaseKeysOnly
+  })
+  expect(_.keys(csvData[0])).toEqual(expectedColumnMapLowercaseKeys)
+})
+test('test load csv works with column map with lowerCase propertyNames and lowerCase mapped column names', async () => {
+  expect.hasAssertions()
+  const csvData: any = await loadCsv({
+    path: testCSVPath,
+    columnNameMap: columnMapAllLowercase
+  })
+  expect(_.keys(csvData[0])).toEqual(expectedColumnMapAllLowercase)
 })
