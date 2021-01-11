11a329eb24546ae92302d81805a2d660fe57a73a
rickjohnson <richardpatrickjohnson@gmail.com>
2021-01-11 16:05:41 -0500

    passing loadPublications test

--- ingest/units/loadPublications.ts
+++ ingest/units/loadPublications.ts
@@ -1,21 +1,57 @@
 import _ from 'lodash'
 import { command as loadCsv } from './loadCsv'
 
-export async function loadPublications (csvPath: string) {
+/**
+ * 
+ * @param csvPath the path the csv file containing the publications to be loaded
+ * 
+ * @param columnNameMap a map of column names in the csv to harvestset property names
+ * 
+ * @returns object with array of raw publication set as well as hash of doi to index of corresponding publication in array
+ */
+export async function loadPublications (csvPath: string, columnNameMap={}): Promise<NormedPublication[]> {
   console.log(`Loading Papers from path: ${csvPath}`)
   // ingest list of DOI's from CSV and relevant center author name
   try {
     const authorPapers: any = await loadCsv({
-     path: csvPath,
-     lowerCaseColumns: true
+      path: csvPath,
+      lowerCaseColumns: true,
+      columnNameMap: columnNameMap
     })
 
-    const papersByDoi = _.groupBy(authorPapers, function(paper) {
-      // strip off 'doi:' if present
-      return _.replace(paper['doi'], 'doi:', '')
+    let sourceName = undefined
+    if (authorPapers.length > 0){
+      sourceName = authorPapers[0]['sourcename']
+    }
+
+    return _.map(authorPapers, (paper) => {
+      let pub: NormedPublication = {
+        title: paper['title'],
+        journalTitle: paper['journaltitle'],
+        doi: _.replace(paper['doi'], 'doi:', ''),
+        publicationDate: paper['publicationdate'],
+        datasourceName: paper['datasourcename'],
+        sourceId: paper['sourceid']
+      }
+      if (paper['searchperson']) {
+        _.set(pub, 'searchPerson', paper['searchperson'])
+      }
+      if (paper['abstract']) {
+        _.set(pub, 'abstract', paper['abstract'])
+      }
+      if (paper['journalissn']) {
+        _.set(pub, 'journalIssn', paper['journalissn'])
+      }
+      if (paper['journaleissn']) {
+        _.set(pub, 'journalEIssn', paper['journaleissn'])
+      }
+      if (paper['sourcemetadata']) {
+        // parse and get rid of any escaped quote characters
+        const sourceMetadata = JSON.parse(paper['sourcemetadata'])
+        _.set(pub, 'sourceMetadata', sourceMetadata)
+      }
+      return pub
     })
-    
-    return papersByDoi
   } catch (error){
     console.log(`Error on paper load for path ${csvPath}, error: ${error}`)
     return undefined
---
+++ ingest/units/test/loadPublications.test.ts
@@ -0,0 +1,81 @@
+import { loadPublications } from '../loadPublications'
+import _ from 'lodash'
+
+const testCSVPath = './test/fixtures/scopus.2019.csv'
+
+const defaultNormedPerson: NormedPerson = {
+  id: 1,
+  familyName: 'Zhang',
+  givenNameInitial: 'S',
+  givenName: 'Suyaun',
+  startDate: new Date('2017-01-01'),
+  endDate: undefined,
+  sourceIds: {
+    scopusAffiliationId: '60021508'
+  }
+}
+
+const defaultSourceName = 'Scopus'
+
+const defaultPubSourceMetadata = {
+  "@_fa":"true",
+  "link":[
+      {"@_fa":"true","@ref":"self","@href":"https://api.elsevier.com/content/abstract/scopus_id/85077122528"},
+      {"@_fa":"true","@ref":"author-affiliation","@href":"https://api.elsevier.com/content/abstract/scopus_id/85077122528?field=author,affiliation"},
+      {"@_fa":"true","@ref":"scopus","@href":"https://www.scopus.com/inward/record.uri?partnerID=HzOxMe3b&scp=85077122528&origin=inward"},
+      {"@_fa":"true","@ref":"scopus-citedby","@href":"https://www.scopus.com/inward/citedby.uri?partnerID=HzOxMe3b&scp=85077122528&origin=inward"}],
+  "prism:url":"https://api.elsevier.com/content/abstract/scopus_id/85077122528",
+  "dc:identifier":"SCOPUS_ID:85077122528",
+  "eid":"2-s2.0-85077122528",
+  "dc:title":"Oxidation-Induced Polymerization of InP Surface and Implications for Optoelectronic Applications",
+  "dc:creator":"Zhang X.",
+  "prism:publicationName":"Journal of Physical Chemistry C",
+  "prism:issn":"19327447",
+  "prism:eIssn":"19327455",
+  "prism:volume":"123",
+  "prism:issueIdentifier":"51",
+  "prism:pageRange":"30893-30902",
+  "prism:coverDate":"2019-12-26",
+  "prism:coverDisplayDate":"26 December 2019",
+  "prism:doi":"10.1021/acs.jpcc.9b07260",
+  "citedby-count":"3",
+  "affiliation":[{"@_fa":"true","affilname":"Notre Dame Radiation Laboratory","affiliation-city":"Notre Dame","affiliation-country":"United States"}],
+  "prism:aggregationType":"Journal",
+  "subtype":"ar",
+  "subtypeDescription":"Article",
+  "source-id":"5200153123",
+  "openaccess":"0",
+  "openaccessFlag":false
+}
+
+const defaultExpectedNormedPublication: NormedPublication = {
+  //searchPerson: defaultNormedPerson,
+  title: 'Oxidation-Induced Polymerization of InP Surface and Implications for Optoelectronic Applications',
+  journalTitle: 'Journal of Physical Chemistry C',
+  doi: '10.1021/acs.jpcc.9b07260',
+  publicationDate: '2019-12-26',
+  datasourceName: defaultSourceName,
+  sourceId: '85077122528',
+  sourceMetadata: defaultPubSourceMetadata,
+  journalIssn: "19327447",
+  journalEIssn: "19327455"
+}
+
+const publicationColumnMap = {
+  // 'search_person': 'searchPerson': NormedPerson,
+  title: 'title',
+  'journal': 'journalTitle',
+  doi: 'doi',
+  'publication_date': 'publicationDate',
+  'source_name': 'datasourceName',
+  'source_id': 'sourceId',
+  'source_metadata': 'sourceMetadata',
+  'journal_issn': 'journalIssn',
+  'journal_eissn': 'journalEIssn'
+}
+
+test('test load publications works with property map with no lowerCase keys or values', async () => {
+  expect.hasAssertions()
+  const publications: NormedPublication[] = await loadPublications(testCSVPath, publicationColumnMap)
+  expect(publications).toEqual(expect.arrayContaining([defaultExpectedNormedPublication]))
+})
