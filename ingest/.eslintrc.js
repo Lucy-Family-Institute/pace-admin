bdf1829127abb6f6a89f259e34e10e8f3d7da659
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2021-01-13 16:48:24 -0500

    Adding no floating promises eslint check
    This relates to https://github.com/share-research/pace-admin/pull/131
    
    To use it,
    
    ```
    $ cd ingest
    $ yarn run eslint /path/to/file
    ```
    
    Then look for the following:
    
    ```
    error    Promises must be handled appropriately or explicitly marked as
    ignored with the `void` operator
    ```
    
    Once we get an ingest README added, I'll update accordingly.

--- ingest/.eslintrc.js
+++ ingest/.eslintrc.js
@@ -3,8 +3,13 @@ module.exports = {
   "parserOptions": {
       "project": "./tsconfig.json"
   },
-  "extends": "standard-with-typescript",
+  "extends": [
+    "standard-with-typescript",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
   "rules": {
-    "@typescript-eslint/strict-boolean-expressions": "off"
+    "@typescript-eslint/strict-boolean-expressions": "off",
+    "@typescript-eslint/no-floating-promises": "error",
   }
 }
