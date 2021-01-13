56b1af0849730635ec8618686ad50138eef5525a
rickjohnson <richardpatrickjohnson@gmail.com>
2021-01-13 14:45:40 -0500

    update normedperson reference in harvester.test.ts

--- ingest/modules/test/harvester.test.ts
+++ ingest/modules/test/harvester.test.ts
@@ -1,13 +1,16 @@
 import { Harvester } from '../harvester'
 import { ScopusDataSource } from '../scopusDataSource'
 import { loadPublications} from '../../units/loadPublications'
+import { loadPersons} from '../../units/loadPersons'
+
 import dotenv from 'dotenv'
 const fs = require('fs');
 import _ from 'lodash'
 
 let scopusHarvester: Harvester
 let defaultNormedPerson: NormedPerson
-let expectedPublications: {}
+let testPersons: NormedPerson[]
+let expectedPublications: any[]
 
 beforeAll(async () => {
 
@@ -35,9 +38,9 @@ beforeAll(async () => {
 
     defaultNormedPerson = {
         id: 1,
-        lastName: 'Zhang',
-        firstInitial: 'S',
-        firstName: 'Suyaun',
+        familyName: 'Zhang',
+        givenNameInitial: 'S',
+        givenName: 'Suyaun',
         startDate: new Date('2017-01-01'),
         endDate: undefined,
         sourceIds: {
@@ -45,18 +48,29 @@ beforeAll(async () => {
         }
     }
 
-    const testPersonsFilePath = '../../../data/persons_202101040758.csv'
-    const expectedPubCSVPath = '../../../data/scopus.2019.20210104110625.csv'
-    expectedPublications = await loadPublications(expectedPubCSVPath)
+    const personPropMap = {
+        id: 'id',
+        givenname: 'given_name',
+        familyname: 'family_name',
+        startdate: 'start_date',
+        enddate: 'end_date'
+    }
+
+    const testPersonsFilePath = '../../test/fixtures/persons_2020.csv'
+    const expectedPubCSVPath = '../../test/fixtures/scopus.2019.20210104110625.csv'
+    expectedPublications = _.values(await loadPublications(expectedPubCSVPath))
+
+    testPersons = await loadPersons(testPersonsFilePath, personPropMap)
 })
 
 //TODO load in list of people to test against expected results for 2019
 test('test Scopus harvester.harvest', async () => {
     expect.hasAssertions()
-    const results = await scopusHarvester.harvest([defaultNormedPerson], new Date('2019-01-01'))
+    const results = await scopusHarvester.harvest(testPersons, new Date('2019-01-01'))
     // as new publications may be added to available, just test that current set includes expected pubs
     // and that total harvested is in the same power of 10 and less than double the expected amount
     console.log(`Found publications: ${results.foundPublications.length}`)
+    expect(results['foundPublications']).toEqual(expect.arrayContaining(expectedPublications))
 })
 
 //TODO write loadPublications test
