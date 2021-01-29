a61aba6713d5b7c5f4c06120a910a6accc3dda6f
rickjohnson <richardpatrickjohnson@gmail.com>
2021-01-29 16:00:05 -0500

    rewrote loadpublications w json config for csv col

--- ingest/units/loadPublications.ts
+++ ingest/units/loadPublications.ts
@@ -1,22 +1,23 @@
 import _ from 'lodash'
 import { command as loadCsv } from './loadCsv'
+import { loadNormedPublicationObjectToCSVMap } from './loadNormedPublicationObjectToCSVMap'
 
+const defaultPublicationColumnMap = loadNormedPublicationObjectToCSVMap()
 /**
  * 
  * @param csvPath the path the csv file containing the publications to be loaded
  * 
- * @param columnNameMap a map of column names in the csv to harvestset property names
+ * @param columnNameMap a map of column names in the csv to harvestset property names, if not defined uses default path from configuration
  * 
  * @returns object with array of raw publication set as well as hash of doi to index of corresponding publication in array
  */
-export async function loadPublications (csvPath: string, columnNameMap={}): Promise<NormedPublication[]> {
+export async function loadPublications (csvPath: string, columnNameMap=defaultPublicationColumnMap): Promise<NormedPublication[]> {
   console.log(`Loading Papers from path: ${csvPath}`)
   // ingest list of DOI's from CSV and relevant center author name
   try {
     const authorPapers: any = await loadCsv({
       path: csvPath,
-      lowerCaseColumns: true,
-      columnNameMap: columnNameMap
+      lowerCaseColumns: true
     })
 
     let sourceName = undefined
@@ -24,48 +25,76 @@ export async function loadPublications (csvPath: string, columnNameMap={}): Prom
       sourceName = authorPapers[0]['sourcename']
     }
 
+    const objectToCSVMap = loadNormedPublicationObjectToCSVMap()
     return _.map(authorPapers, (paper) => {
-      let pub: NormedPublication = {
-        title: paper['title'],
-        journalTitle: paper['journaltitle'],
-        doi: _.replace(paper['doi'], 'doi:', ''),
-        publicationDate: paper['publicationdate'],
-        datasourceName: paper['datasourcename'],
-        sourceId: paper['sourceid']
-      }
-      if (paper['searchperson']) {
-        _.set(pub, 'searchPerson', paper['searchperson'])
-      } else if (paper['searchpersonfamilyname']) {
-        const person: NormedPerson = {
-          id: Number.parseInt(paper['searchpersonid']),
-          familyName: paper['searchpersonfamilyname'],
-          givenName: paper['searchpersongivenname'],
-          givenNameInitial: paper['searchpersongivennameinitial'],
-          startDate: (paper['searchpersonstartdate'] ? new Date(paper['searchpersonstartdate']) : undefined),
-          endDate: (paper['searchpersonenddate'] ? new Date(paper['searchpersonenddate']) : undefined),
-          sourceIds: (paper['searchpersonsourceidsscopusaffiliationid'] ? 
-            { scopusAffiliationId: paper['searchpersonsourceidsscopusaffiliationid'] } : {})
-        }
-        _.set(pub, 'searchPerson', person)
-      }
-      if (paper['abstract']) {
-        _.set(pub, 'abstract', paper['abstract'])
-      }
-      if (paper['journalissn']) {
-        _.set(pub, 'journalIssn', paper['journalissn'])
-      }
-      if (paper['journaleissn']) {
-        _.set(pub, 'journalEIssn', paper['journaleissn'])
-      }
-      if (paper['sourcemetadata']) {
-        // parse and get rid of any escaped quote characters
-        const sourceMetadata = JSON.parse(paper['sourcemetadata'])
-        _.set(pub, 'sourceMetadata', sourceMetadata)
-      }
-      return pub
+      return getNormedPublicationObjectFromCSVRow(paper, objectToCSVMap)
     })
   } catch (error){
     console.log(`Error on paper load for path ${csvPath}, error: ${error}`)
     return undefined
   }
+}
+
+/**
+ * Expects the map to be used in defining column_names to pull properties for each leaf of NormedPublication object 
+ * (e.g., for the searchPerson property there is an object that defines a column name for each item that equates to a string)
+ * @param row 
+ */
+export function getNormedPublicationObjectFromCSVRow(row, objectToCSVMap): NormedPublication {
+  // assumes all column names in row passed in have been converted to lowercase
+  const searchPersonFamilyNameColumn = objectToCSVMap['searchPerson']['familyName']
+  let pub: NormedPublication = {
+
+    searchPerson: {
+      // essentially is the object path referenced as a map
+      id: Number.parseInt(row[_.toLower(objectToCSVMap['searchPerson']['id'])]),
+      familyName: row[_.toLower(objectToCSVMap['searchPerson']['familyName'])],
+      givenNameInitial: row[_.toLower(objectToCSVMap['searchPerson']['givenNameInitial'])],
+      givenName: row[_.toLower(objectToCSVMap['searchPerson']['givenName'])],
+      startDate: new Date(row[_.toLower(objectToCSVMap['searchPerson']['startDate'])]),
+      endDate: new Date(row[_.toLower(objectToCSVMap['searchPerson']['startDate'])]),
+      sourceIds: {
+        scopusAffiliationId: row[_.toLower(objectToCSVMap['searchPerson']['sourceIds']['scopusAffiliationId'])]
+      }
+    },
+    title: row[_.toLower(objectToCSVMap['title'])],
+    journalTitle: row[_.toLower(objectToCSVMap['journalTitle'])],
+    doi: row[_.toLower(objectToCSVMap['doi'])],
+    publicationDate: row[_.toLower(objectToCSVMap['publicationDate'])],
+    datasourceName: row[_.toLower(objectToCSVMap['datasourceName'])]
+  }
+  // set optional properties, for search person first check if family name provided
+  if (row[_.toLower(searchPersonFamilyNameColumn)]){
+    const person: NormedPerson = {
+      id: row[_.toLower(objectToCSVMap['searchPerson']['id'])] ? Number.parseInt(row[_.toLower(objectToCSVMap['searchPerson']['id'])]) : undefined,
+      familyName: row[_.toLower(searchPersonFamilyNameColumn)],
+      givenName: row[_.toLower(objectToCSVMap['searchPerson']['givenName'])] ? row[_.toLower(objectToCSVMap['searchPerson']['givenName'])] : undefined,
+      givenNameInitial: row[_.toLower(objectToCSVMap['searchPerson']['givenNameInitial'])] ? row[_.toLower(objectToCSVMap['searchPerson']['givenNameInitial'])] : undefined,
+      startDate: row[_.toLower(objectToCSVMap['searchPerson']['startDate'])] ? new Date(row[_.toLower(objectToCSVMap['searchPerson']['startDate'])]) : undefined,
+      endDate: row[_.toLower(objectToCSVMap['searchPerson']['endDate'])] ? new Date(row[_.toLower(objectToCSVMap['searchPerson']['endDate'])]) : undefined,
+      sourceIds: row[_.toLower(objectToCSVMap['searchPerson']['sourceIds']['scopusAffiliationId'])] ? 
+        { scopusAffiliationId: row[_.toLower(objectToCSVMap['searchPerson']['sourceIds']['scopusAffiliationId'])] } : {}
+    }
+    _.set(pub, 'searchPerson', person)
+  }
+
+  if (row[_.toLower(objectToCSVMap['abstract'])]) {
+    _.set(pub, 'abstract', row[_.toLower(objectToCSVMap['abstract'])])
+  }
+  if (row[_.toLower(objectToCSVMap['journalIssn'])]) {
+    _.set(pub, 'journalIssn', row[_.toLower(objectToCSVMap['journalIssn'])])
+  }
+  if (row[_.toLower(objectToCSVMap['journalEIssn'])]) {
+    _.set(pub, 'journalEIssn', row[_.toLower(objectToCSVMap['journalEIssn'])])
+  }
+  if (row[_.toLower(objectToCSVMap['sourceId'])]) {
+    _.set(pub, 'sourceId', row[_.toLower(objectToCSVMap['sourceId'])])
+  }
+  if (row[_.toLower(objectToCSVMap['sourceMetadata'])]) {
+    // parse and get rid of any escaped quote characters
+    const sourceMetadata = JSON.parse(row[_.toLower(objectToCSVMap['sourceMetadata'])])
+    _.set(pub, 'sourceMetadata', sourceMetadata)
+  }
+
+  return pub
 }
