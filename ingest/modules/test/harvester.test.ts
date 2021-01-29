3a246978862c5c671ec496f966414e8b1dfe356d
rickjohnson <richardpatrickjohnson@gmail.com>
2021-01-29 10:40:15 -0500

    scopus harvest test now confirms all pubs returned

--- ingest/modules/test/harvester.test.ts
+++ ingest/modules/test/harvester.test.ts
@@ -132,13 +132,42 @@ test('test Scopus harvester.harvest by author name', async () => {
         pageSize: Number.parseInt(scopusConfig.pageSize),
         totalResults: 198
     }
-    const results = await scopusHarvester.harvest(testPersons, HarvestOperation.QUERY_BY_AUTHOR_NAME, new Date('2019-01-01'))
+
+    let expectedHarvestSetArraySize = parseInt(`${expectedHarvestSet.totalResults.valueOf() / expectedHarvestSet.pageSize.valueOf()}`) //convert to an integer to drop any decimal
+    if ((expectedHarvestSet.totalResults.valueOf() % expectedHarvestSet.pageSize.valueOf()) > 0) {
+      expectedHarvestSetArraySize += 1
+    }
+    const results: HarvestSet[] = await scopusHarvester.harvest(testPersons, HarvestOperation.QUERY_BY_AUTHOR_NAME, new Date('2019-01-01'))
     // as new publications may be added to available, just test that current set includes expected pubs
     // and that total harvested is in the same power of 10 and less than double the expected amount
-    //expect(results[0].normedPublications).toEqual(expect.arrayContaining(expectedHarvestSet.normedPublications))
-    //expect(expectedHarvestSet.normedPublications).toEqual(expect.arrayContaining(results[0].normedPublications))
-    //expect(results[0].sourcePublications[0]).toEqual('test')
-    expect(results[0].normedPublications[0]).toEqual(expectedHarvestSet.normedPublications[0])
+    expect(results.length).toEqual(expectedHarvestSetArraySize) // checking the right number of harvest sets return (in chunks based on page size)
+    // combine values
+    const combinedNormedPubs = _.mapValues(results, (result:HarvestSet) => {
+        return result.normedPublications
+    })
+    
+    let resultNormedPubs = []
+    _.each(combinedNormedPubs, (pubs, index) => {
+        resultNormedPubs = _.concat(resultNormedPubs, pubs)
+    })
+    const resultNormedPubsByDoi = _.mapKeys(resultNormedPubs, (normedPub) => {
+        return normedPub['doi']
+    })
+    const expectedNormedPubsByDoi = _.mapKeys(expectedHarvestSet.normedPublications, (expectedPub) => {
+        return expectedPub['doi']
+    })
+    expect(resultNormedPubs.length).toBeGreaterThanOrEqual(expectedHarvestSet.totalResults.valueOf())
+    // check for each expected pub
+    _.each(_.keys(expectedNormedPubsByDoi), (doi) => {
+
+         // ignore sourcemetadata since things like citedby-count often change over time
+        const expectedPub = _.omit(expectedNormedPubsByDoi[doi], 'sourceMetadata')
+        const receivedPub = _.omit(resultNormedPubsByDoi[doi], 'sourceMetadata')
+       
+        expect(expectedPub).toEqual(receivedPub)
+        // finally just check that source metadata is defined
+        expect(resultNormedPubsByDoi[doi]['sourceMetadata']).toBeDefined()
+    })
 })
 
 //TODO load in list of people to test against expected results for 2019
@@ -159,5 +188,3 @@ test('test Scopus harvester.harvest by author id throws error', async () => {
     // and that total harvested is in the same power of 10 and less than double the expected amount
     expect(results[0].errors).toEqual(['\'QUERY_BY_AUTHOR_ID\' not supported by datasource harvester Scopus'])
 })
-
-//TODO write loadPublications test
