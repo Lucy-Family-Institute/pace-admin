50f4f909777fec4f279d162ae1f54ef018fd4939
rickjohnson <richardpatrickjohnson@gmail.com>
2021-07-23 16:15:44 -0400

    add get bibtex properties from WoS

--- ingest/modules/wosDataSource.ts
+++ ingest/modules/wosDataSource.ts
@@ -2,6 +2,7 @@ import axios, { AxiosResponse } from 'axios'
 import _ from 'lodash'
 const xmlToJson = require('xml-js');
 import NormedPublication from './normedPublication'
+import NormedAuthor from './normedAuthor'
 import NormedPerson from './normedPerson'
 import HarvestSet from './HarvestSet'
 import DataSource from './dataSource'
@@ -185,26 +186,36 @@ export class WosDataSource implements DataSource {
    */
   getWoSMapLabelsToValues(properties) {
     let transformedProperties = {}
-    _.each(properties, (property) => {
+    console.log(`properties are: ${JSON.stringify(properties, null, 2)}`)
+    let parseProperties = properties
+    if (!_.isArray(properties)){
+      parseProperties = [properties]
+    }
+    _.each(parseProperties, (property) => {
       transformedProperties[property['label']['_text']] = property['value']
     })
     return transformedProperties
   }
 
   getAuthors(sourceMetadata) {
+    console.log(`Getting authors from source metadata: ${JSON.stringify(sourceMetadata, null, 2)}`)
     let authors = []
-    if (sourceMetadata && sourceMetadata['authors'] && sourceMetadata['authors'].length > 0 && sourceMetadata['authors'][0]['value']){
-      _.each(sourceMetadata['authors'][0]['value'], (author) => {
-        authors.push({name: author['_text']})
-      })
+    if (sourceMetadata && sourceMetadata['authors']){
+      const sourceAuthors = this.getWoSMapLabelsToValues(sourceMetadata['authors'])
+      if (sourceAuthors['Authors'] && sourceAuthors['Authors'].length > 0){
+        _.each(sourceAuthors['Authors'], (author) => {
+          authors.push({name: author['_text']})
+        })
+      }
     }
     return authors
   }
 
   async getCSLStyleAuthorList(sourceMetadata) {
     const sourceAuthors = this.getAuthors(sourceMetadata)
+    console.log(`source authors are: ${JSON.stringify(sourceAuthors, null, 2)}`)
     const cslStyleAuthors = []
-    await pMap(sourceAuthors, async (sourceAuthor, index) => {
+    await pMap (sourceAuthors, async (sourceAuthor, index) => {
       let author = {}
       const parsedName = await nameParser({
         name: sourceAuthor['name'],
@@ -217,9 +228,28 @@ export class WosDataSource implements DataSource {
     return cslStyleAuthors
   }
 
+  async getNormedAuthors(sourceMetadata): Promise<NormedAuthor[]> {
+    const cslAuthors = await this.getCSLStyleAuthorList(sourceMetadata)
+    console.log(`CSL authors are: ${JSON.stringify(cslAuthors, null, 2)}`)
+    const normedAuthors: NormedAuthor[] = []
+    _.each(cslAuthors, (sourceAuthor, index) => {
+      let author: NormedAuthor = {
+        familyName: sourceAuthor.family,
+        givenName: sourceAuthor.given,
+        givenNameInitial: sourceAuthor.given[0],
+        affiliations: sourceAuthor.affiliation,
+        sourceIds: { }
+      }
+      normedAuthors.push(author)
+    })
+    console.log(`Normed authors are: ${JSON.stringify(normedAuthors, null, 2)}`)
+    return normedAuthors
+  }
+
   // returns an array of normalized publication objects given ones retrieved fron this datasource
-  getNormedPublications(sourcePublications: any[], searchPerson?: NormedPerson): NormedPublication[]{
-    return _.map(sourcePublications, (pub) => {
+  async getNormedPublications(sourcePublications: any[], searchPerson?: NormedPerson): Promise<NormedPublication[]>{
+    let normedPubs = []
+    await pMap (sourcePublications, async (pub) => {
       const otherProps = this.getWoSMapLabelsToValues(pub['other'])
       const sourceProps = this.getWoSMapLabelsToValues(pub['source'])
       
@@ -230,6 +260,7 @@ export class WosDataSource implements DataSource {
             datasourceName: this.dsConfig.sourceName,
             doi: otherProps && otherProps['Identifier.Doi'] && otherProps['Identifier.Doi']['_text'] ? otherProps['Identifier.Doi']['_text'] : '',
             sourceId: pub['uid'] && pub['uid']['_text'] ? `${Number.parseInt(_.replace(pub['uid']['_text'], 'WOS:', ''))}` : '',
+            authors: await this.getNormedAuthors(pub),
             sourceMetadata: pub
         }
         // add optional properties
@@ -240,8 +271,13 @@ export class WosDataSource implements DataSource {
         // don't worry about abstract for now
         if (otherProps && otherProps['Identifier.Issn'] && otherProps['Identifier.Issn']['_text']) _.set(normedPub, 'journalIssn', otherProps['Identifier.Issn']['_text'])
         if (otherProps && otherProps['Identifier.Eissn'] && otherProps['Identifier.Eissn']['_text']) _.set(normedPub, 'journalEIssn', otherProps['Identifier.Eissn']['_text'])
-        return normedPub
-    })
+        
+        if (sourceProps && sourceProps['Issue'] && sourceProps['Issue']['_text']) _.set(normedPub, 'number', sourceProps['Issue']['_text'])
+        if (sourceProps && sourceProps['Volume'] && sourceProps['Volume']['_text']) _.set(normedPub, 'volume', sourceProps['Volume']['_text'])
+        if (sourceProps && sourceProps['Pages'] && sourceProps['Pages']['_text']) _.set(normedPub, 'pages', sourceProps['Pages']['_text'])
+        normedPubs.push(normedPub)
+    }, { concurrency: 1 })
+    return normedPubs
   }
 
   //returns a machine readable string version of this source
