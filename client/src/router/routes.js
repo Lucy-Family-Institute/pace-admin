1094dafc1ba04685e515b15104da69698ae48452
Jeffrey Spies <code@jeffspies.com>
2021-08-18 11:59:23 -0400

    adding routes back in

--- client/src/router/routes.js
+++ client/src/router/routes.js
@@ -71,12 +71,17 @@ export function makeRoutes (store) {
           component: () => import('pages/Dashboard.vue')
         }
       ]
+    },
+    {
+      path: '/center_review/',
+      component: () => import('layouts/Base.vue'),
+      children: [
+        {
+          path: '',
+          component: () => import('pages/CenterReview.vue')
+        }
+      ]
     }
-    // { path: 'logs', component: () => import('pages/Index.vue') },
-    // { path: 'dashboard', component: () => import('pages/Dashboard.vue') },
-    // { path: 'center_review', component: () => import('pages/CenterReview.vue') }
-    //   ]
-    // }
   ]
 
   // Always leave this as last one
