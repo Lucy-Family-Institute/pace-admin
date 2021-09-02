6ecc26977ad1b750f3446106130517cb24b880b2
Jeffrey Spies <code@jeffspies.com>
2021-09-02 16:02:03 -0400

    downloaded nd assets to better optimize them

--- client/src/pages/PublicLanding.vue
+++ client/src/pages/PublicLanding.vue
@@ -183,7 +183,7 @@
 </template>
 
 <style scoped>
-@import 'https://conductor.nd.edu/stylesheets/themes/ndt/v3/ndt.css';
+@import '../statics/edu.nd/conductor.nd.edu/stylesheets/themes/ndt/v3/ndt.css';
 @import '../statics/ndweb/stylesheets/site.css';
 p.page-image img {
   border: 1px #e6e6e6 solid;
