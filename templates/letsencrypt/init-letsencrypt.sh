1c611fe3ccd1c20b79bdffb6db12a5374d8a39a0
rickjohnson <richardpatrickjohnson@gmail.com>
2021-09-09 13:14:27 -0400

    update with pace.nd.edu address

--- templates/letsencrypt/init-letsencrypt.sh
+++ templates/letsencrypt/init-letsencrypt.sh
@@ -6,7 +6,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
 fi
 
 domains=()
-domains+="paceadmin.zapto.org"
+domains+="pace.nd.edu"
 rsa_key_size=4096
 data_path="./build/certbot"
 email="rjohns14@nd.edu" # Adding a valid address is strongly recommended
