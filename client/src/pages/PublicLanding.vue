bf03295a161b7ea229a55f00210bd69996d62fbb
Jeffrey Spies <code@jeffspies.com>
2021-09-09 01:59:54 -0400

    button fix

--- client/src/pages/PublicLanding.vue
+++ client/src/pages/PublicLanding.vue
@@ -151,6 +151,7 @@
                 size="lg"
                 label="EXPLORE PARTNERSHIP" />&nbsp;
               <q-btn
+                id="login-button"
                 type="a"
                 href="/login"
                 color="primary"
@@ -263,6 +264,15 @@ main {
   unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
   font-display: swap;
 }
+
+.q-btn {
+  margin-top: 5px;
+}
+
+/* .buttons {
+  white-space: nowrap;
+} */
+
 </style>
 
 <script>
