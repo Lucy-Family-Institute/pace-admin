cb86c5d15f90244f54239ca7a2c315bf377fce23
rickjohnson <richardpatrickjohnson@gmail.com>
2021-10-04 09:50:25 -0400

    remove select columns slowing down pub read query

--- ingest/gql/readPublicationsFromStartYear.js
+++ ingest/gql/readPublicationsFromStartYear.js
@@ -9,15 +9,8 @@ export default function readPublicationsFromStartYear (startYear) {
           title
           doi
           year
-          csl_string
           csl
           source_name
-          source_metadata
-          scopus_eid: source_metadata(path: "eid")
-          scopus_pii: source_metadata(path: "pii")
-          wos_id: source_metadata(path: "uid")
-          pubmed_resource_identifiers: source_metadata(path: "resourceIdentifiers")
-          journal_title: csl(path: "container-title")
         }
       }
     `,
