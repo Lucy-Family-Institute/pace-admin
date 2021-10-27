beaafffb8a90c337f2136a7cad0575325a0504a4
rickjohnson <richardpatrickjohnson@gmail.com>
2021-10-27 16:10:28 -0400

    add user organizations from user seed csv

--- node-admin-client/addUsersByCsv.ts
+++ node-admin-client/addUsersByCsv.ts
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv'
-import { User, UserRole, PaceClient, PaceClientConfig } from './src/client'
+import { DBUser, User, UserOrg, UserRole, PaceClient, PaceClientConfig } from './src/client'
 import { command as loadCsv } from '../ingest/units/loadCsv' 
 import _ from 'lodash'
 
@@ -31,6 +31,7 @@ import _ from 'lodash'
   if (addCsvUsers) {
     console.log(`Trying to add csv users ${JSON.stringify(addCsvUsers, null, 2)}...`)
     let userRoles = []
+    let addUserOrgsByEmail = {}
     const users = _.map(addCsvUsers, (addUser) => {
       console.log(`Starting prep user ${addUser}`)
       let password = addUser['password']
@@ -49,6 +50,9 @@ import _ from 'lodash'
         role: addUser['role']
       }
       userRoles.push(userRole)
+      if (addUser['organizations'] && addUser['organizations'].length > 0){
+        addUserOrgsByEmail[addUser['email']] = addUser['organizations']
+      }
       return user
     })
     await client.registerUsers(users)
@@ -56,5 +60,33 @@ import _ from 'lodash'
     console.log(`Trying to add user roles...`)
     await client.registerUserRoles(userRoles)    
     console.log(`Finished adding users and roles.`)
+
+    // now add user orgs
+    console.log(`Trying to add user orgs...`)
+    // select users and existing user orgs
+    const dbUsers: DBUser[] = await client.getDBUsers()
+    // match email to user
+    const usersByEmail = _.mapKeys(dbUsers, (dbUser: DBUser) => {
+      return dbUser.email
+    })
+    // construct userid and org objects
+    let insertUserOrgs: UserOrg[] = []
+    _.each(_.keys(addUserOrgsByEmail), (email) => {
+      if (usersByEmail[email]){
+        const dbUser: DBUser = usersByEmail[email]
+        const addUserOrgs = _.split(addUserOrgsByEmail[email], ';')
+        _.each(addUserOrgs, (addOrg) => {
+          const userOrg: UserOrg = {
+            userId: dbUser.id,
+            org: addOrg
+          }
+          insertUserOrgs.push(userOrg)
+        })
+      } else {
+        console.log(`Warning skipping add user org not in DB email: '${email}'', orgs: '${addUserOrgsByEmail[email]}'`)
+      }
+    })
+    await client.registerUserOrgs(insertUserOrgs)
+    console.log(`Finished adding orgs.`)
   }
 })()
