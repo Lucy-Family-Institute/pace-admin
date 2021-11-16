24021c476a0bb4e258b942c40fe76d0464e35860
rickjohnson <richardpatrickjohnson@gmail.com>
2021-11-16 11:58:33 -0500

    remove dup migration causing it to fail

--- hasura/migrations/1635275160144_insert_user_role_enums/down.sql
+++
@@ -1,6 +0,0 @@
--- Could not auto-generate a down migration.
--- Please write an appropriate down migration for the SQL below:
--- insert into public.users_roles values ('VIEWER', 'View-only user role');
--- insert into public.users_roles values ('LIBRARY-REVIEWER', 'Author review only user');
--- insert into public.users_roles values ('CENTER-REVIEWER', 'Center/Institute review only user');
--- insert into public.users_roles values ('ADMIN', 'Admin user that can access all tabs');
